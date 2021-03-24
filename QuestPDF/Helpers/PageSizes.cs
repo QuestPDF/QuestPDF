1a26febf081d368480da5fcb26db5c604cb63d59
Laurence Mingle <lmingle@users.noreply.github.com>
2021-03-24 15:40:46 -0400

    Update PageSizes.cs
    Formatting

--- QuestPDF/Helpers/PageSizes.cs
+++ QuestPDF/Helpers/PageSizes.cs
@@ -29,21 +29,21 @@ namespace QuestPDF.Helpers
         public static Size A7 => new Size(210, 297);
         public static Size A8 => new Size(148, 210);
         public static Size A9 => new Size(105, 148);
-        public static Size A10 => new Size(73, 105);		
-		public static Size AnsiA => new Size(792, 612);
-		public static Size AnsiB => new Size(1224,792);
+        public static Size A10 => new Size(73, 105);
+        public static Size AnsiA => new Size(792, 612);
+        public static Size AnsiB => new Size(1224,792);
         public static Size AnsiC => new Size(1224, 1584);
         public static Size AnsiD => new Size(1584, 2448);
-        public static Size AnsiE => new Size(2448, 3168);		
+        public static Size AnsiE => new Size(2448, 3168);
         public static Size ARCHA => new Size(648, 864);
         public static Size ARCHB => new Size(864, 1296);
         public static Size ARCHC => new Size(1296, 1728);
         public static Size ARCHD => new Size(1728, 2592);
         public static Size ARCHE => new Size(2592, 3456);
-		public static Size ARCH_E1 => new Size(2160, 3025);
-		public static Size ARCH_E2 => new Size(1871, 2735);
-		public static Size ARCH_E3 => new Size(1945, 2809);
-		
+        public static Size ARCH_E1 => new Size(2160, 3025);
+        public static Size ARCH_E2 => new Size(1871, 2735);
+        public static Size ARCH_E3 => new Size(1945, 2809);
+
         public static Size B0 => new Size(2920, 4127);
         public static Size B1 => new Size(2064, 2920);
         public static Size B2 => new Size(1460, 2064);
@@ -59,21 +59,21 @@ namespace QuestPDF.Helpers
         public static Size B8 => new Size(181, 258);
         public static Size B9 => new Size(127, 181);
         public static Size B10 => new Size(91, 127);
-		
-		public static Size C2 => new Size(1298, 1837);
-		public static Size C3 => new Size(918, 1298);
+
+        public static Size C2 => new Size(1298, 1837);
+        public static Size C3 => new Size(918, 1298);
         public static Size C4 => new Size(649, 918);
         public static Size C5 => new Size(459, 649);
         public static Size C6 => new Size(323, 459);
-		
-		public static Size D0 => new Size(3090, 2186);
-		
+
+        public static Size D0 => new Size(3090, 2186);
+
         public static Size Comm10 => new Size(297, 684);
-		
+
         public static Size DL => new Size(312, 624);
         public static Size DoublePostcard => new Size(567, 419.5f);
         public static Size DoublePostcardRotated => new Size(419.5f, 567);
-		
+
         public static Size Env9 => new Size(279, 639);
         public static Size Env10 => new Size(297, 684);
         public static Size Env11 => new Size(324, 747);
@@ -125,14 +125,14 @@ namespace QuestPDF.Helpers
         public static Size EnvPRC10 => new Size(918, 1298);
         public static Size EnvPRC10Rotated => new Size(1298, 918);
         public static Size EnvYou4 => new Size(298, 666);
-        public static Size EnvYou4Rotated => new Size(666, 298);		
+        public static Size EnvYou4Rotated => new Size(666, 298);
         public static Size Executive => new Size(522, 756);
-		
+
         public static Size FanFoldUS => new Size(1071, 792);
         public static Size FanFoldGerman => new Size(612, 864);
         public static Size FanFoldGermanLegal => new Size(612, 936);
         public static Size Folio => new Size(595, 935);
-		
+
         public static Size ISOB0 => new Size(2835, 4008);
         public static Size ISOB1 => new Size(2004, 2835);
         public static Size ISOB2 => new Size(1417, 2004);
@@ -145,7 +145,7 @@ namespace QuestPDF.Helpers
         public static Size ISOB8 => new Size(176, 249);
         public static Size ISOB9 => new Size(125, 176);
         public static Size ISOB10 => new Size(88, 125);
-		
+
         public static Size Ledger => new Size(1224, 792);
         public static Size Legal => new Size(612, 1008);
         public static Size LegalExtra => new Size(684, 1080);
@@ -156,24 +156,24 @@ namespace QuestPDF.Helpers
         public static Size LetterPlus => new Size(612, 913.7f);
         public static Size LetterRotated => new Size(792, 612);
         public static Size LetterSmall => new Size(612, 792);
-		
+
         public static Size Monarch => new Size(279, 540);
-		
+
         public static Size Note => new Size(612, 792);
-		
+
         public static Size Postcard => new Size(284, 419);
         public static Size PostcardRotated => new Size(419, 284);
-		
+
         public static Size PRC16K => new Size(414, 610);
         public static Size PRC16KRotated => new Size(610, 414);
         public static Size PRC32K => new Size(275, 428);
         public static Size PRC32KBig => new Size(275, 428);
         public static Size PRC32KBigRotated => new Size(428, 275);
         public static Size PRC32KRotated => new Size(428, 275);
-		
+
         public static Size Quarto => new Size(610, 780);
-		
-		public static Size Standard10x11 => new Size(720, 792);
+
+        public static Size Standard10x11 => new Size(720, 792);
         public static Size Standard10x13 => new Size(720, 936);
         public static Size Standard10x14 => new Size(720, 1008);
         public static Size Standard12x11 => new Size(864, 792);
@@ -185,7 +185,7 @@ namespace QuestPDF.Helpers
         public static Size Statement => new Size(396, 612);
         public static Size SuperA => new Size(643, 1009);
         public static Size SuperB => new Size(864, 1380);
-		
+
         public static Size Tabloid => new Size(792, 1224);
         public static Size TabloidExtra => new Size(864, 1296);
     }
