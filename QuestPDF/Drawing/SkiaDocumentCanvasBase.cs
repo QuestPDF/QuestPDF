79cc5f1507dbd4335801a1803f74f8250d20d587
Marcin Ziąbek <marcin.ziabek@uj.edu.pl>
2021-11-19 00:22:45 +0100

    Added support for XPS rendering

---
+++ QuestPDF/Drawing/SkiaDocumentCanvasBase.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using QuestPDF.Infrastructure;
+using SkiaSharp;
+
+namespace QuestPDF.Drawing
+{
+    internal class SkiaDocumentCanvasBase : SkiaCanvasBase
+    {
+        private SKDocument Document { get; }
+
+        public SkiaDocumentCanvasBase(SKDocument document)
+        {
+            Document = document;
+        }
+
+        ~SkiaDocumentCanvasBase()
+        {
+            Document.Dispose();
+        }
+        
+        public override void BeginDocument()
+        {
+            
+        }
+
+        public override void EndDocument()
+        {
+            Canvas.Dispose();
+            
+            Document.Close();
+            Document.Dispose();
+        }
+
+        public override void BeginPage(Size size)
+        {
+            Canvas = Document.BeginPage(size.Width, size.Height);
+        }
+
+        public override void EndPage()
+        {
+            Document.EndPage();
+            Canvas.Dispose();
+        }
+    }
+}
