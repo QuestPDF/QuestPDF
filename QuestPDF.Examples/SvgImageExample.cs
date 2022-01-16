e3ce194fd152cfe66daa63cea436a0dfc78da4c3
Marcin Ziąbek <marcin.ziabek@uj.edu.pl>
2022-01-16 17:03:01 +0100

    Simple SVG example

---
+++ QuestPDF.Examples/SvgImageExample.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using QuestPDF.Examples.Engine;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using SkiaSharp;
+using Svg.Skia;
+
+namespace QuestPDF.Examples
+{
+    public class SvgImageExample
+    {
+        [Test]
+        public void BorderRadius()
+        {
+            RenderingTest
+                .Create()
+                .PageSize(175, 100)
+                .ProduceImages()
+                .ShowResults()
+                .Render(container =>
+                {
+                    container
+                        .Background(Colors.Grey.Lighten2)
+                        .Padding(25)
+                        .Canvas((canvas, space) =>
+                        {
+                            using var svg = new SKSvg();
+                            svg.Load("pdf-icon.svg");
+                            
+                            canvas.DrawPicture(svg.Picture);
+                        });
+                });
+        }
+    }
+}
