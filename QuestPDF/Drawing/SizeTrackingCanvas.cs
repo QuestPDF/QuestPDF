726759f47aa467244e6fc0ac4187330091b7e355
Bebo-Maker <bennetbo@gmx.de>
2022-03-16 22:45:57 +0100

    Add size tracking canvas.

---
+++ QuestPDF/Drawing/SizeTrackingCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.Drawing
+{
+    internal class SizeTrackingCanvas : FreeCanvas
+    {
+        private readonly List<Size> _pageSizes = new();
+        public IReadOnlyList<Size> PageSizes => _pageSizes;
+
+        public override void BeginPage(Size size)
+        {
+            _pageSizes.Add(size);
+        }
+    }
+}
