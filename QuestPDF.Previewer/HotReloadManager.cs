8217b3b54a90f03986b1744784ec979bf916c709
Bebo-Maker <bennetbo@gmx.de>
2022-03-16 22:46:21 +0100

    Implement HotReloadManager.

---
+++ QuestPDF.Previewer/HotReloadManager.cs
@@ -0,0 +1,30 @@
+﻿[assembly: System.Reflection.Metadata.MetadataUpdateHandler(typeof(QuestPDF.Previewer.HotReloadManager))]
+
+namespace QuestPDF.Previewer
+{
+    /// <summary>
+    /// Helper for subscribing to hot reload notifications.
+    /// </summary>
+    internal static class HotReloadManager
+    {
+        private static readonly List<Action> _actions = new();
+
+        public static void Register(Action action)
+        {
+            _actions.Add(action);
+        }
+
+        public static void Unregister(Action action)
+        {
+            _actions.Remove(action);
+        }
+
+        public static void ClearCache(Type[]? _) { }
+
+        public static void UpdateApplication(Type[]? _)
+        {
+            foreach (var action in _actions)
+                action();
+        }
+    }
+}
