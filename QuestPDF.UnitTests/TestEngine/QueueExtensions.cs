8649a96a86b15ba0dccce406e362edff49925fc5
Rustam Sayfutdinov <rstm.sf@gmail.com>
2022-10-09 09:34:00 +0300

    Add build and unit tests action

---
+++ QuestPDF.UnitTests/TestEngine/QueueExtensions.cs
@@ -0,0 +1,22 @@
+﻿#if NETFRAMEWORK
+
+using System.Collections.Generic;
+
+namespace QuestPDF.UnitTests.TestEngine;
+
+internal static class QueueExtensions
+{
+    public static bool TryDequeue<T>(this Queue<T> queue, out T result)
+    {
+        if (queue.Count == 0)
+        {
+            result = default!;
+            return false;
+        }
+
+        result = queue.Dequeue();
+        return true;
+    }
+}
+
+#endif
