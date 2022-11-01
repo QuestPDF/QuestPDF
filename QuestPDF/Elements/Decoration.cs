2a0e91af6846a6ce373d21e0a94005b54aec4acd
MarcinZiabek <marcin@ziabek.com>
2022-11-01 21:32:04 +0100

    Fixed: the Decoration element is not aligned to right when in RTL mode

--- QuestPDF/Elements/Decoration.cs
+++ QuestPDF/Elements/Decoration.cs
@@ -68,7 +68,7 @@ namespace QuestPDF.Elements
                 
                 var offset = ContentDirection == ContentDirection.LeftToRight
                     ? command.Offset
-                    : new Position(availableSpace.Width - command.Offset.X - command.Measurement.Width, command.Offset.Y);
+                    : new Position(availableSpace.Width - width, command.Offset.Y);
                 
                 Canvas.Translate(offset);
                 command.Element.Draw(elementSize);
