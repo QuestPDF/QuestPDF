164bdd76d8a7b961add7ed509dc6b5398eb7a412
MarcinZiabek <marcin@ziabek.com>
2022-11-24 22:44:31 +0100

    Added detector that checks if code is being run via unit test

---
+++ QuestPDF/Previewer/UnitTestDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace QuestPDF.Previewer;
+
+internal static class UnitTestDetector
+{
+    public static bool RunningInUnitTest { get; }   
+
+    private static string[] UnitTestAssemblies = { "xunit", "nunit", "mstest" };
+    
+    static UnitTestDetector()
+    {
+        RunningInUnitTest = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .Select(x => x.FullName.ToLowerInvariant())
+            .Any(x => UnitTestAssemblies.Any(x.Contains));
+    }
+}
