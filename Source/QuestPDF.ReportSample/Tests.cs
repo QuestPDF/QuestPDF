68e4a10e81bd0e5257a8f5a7bedb4e8dc3d2fa7b
MarcinZiabek <marcin@ziabek.com>
2023-01-29 21:59:47 +0100

    Deleted obsolete performance tests

--- Source/QuestPDF.ReportSample/Tests.cs
+++ Source/QuestPDF.ReportSample/Tests.cs
@@ -38,20 +38,5 @@ namespace QuestPDF.ReportSample
             Report.GenerateXps(path);
             Process.Start("explorer.exe", path);
         }
-        
-        [Test] 
-        public void Profile()
-        {
-            ImagePlaceholder.Solid = true;
-            
-            var container = new DocumentContainer();
-            Report.Compose(container);
-            var content = container.Compose();
-            
-            var pageContext = new PageContext();
-
-            DocumentGenerator.RenderPass(pageContext, new FreeCanvas(), content, null);
-            DocumentGenerator.RenderPass(pageContext, new FreeCanvas(), content, null);
-        }
     }
 }
