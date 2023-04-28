f51f2536abf6aa1ee465867886bea98437a3387a
MarcinZiabek <marcin@ziabek.com>
2023-04-28 14:13:43 +0200

    Adjusted license information

--- Source/QuestPDF/Infrastructure/LicenseType.cs
+++ Source/QuestPDF/Infrastructure/LicenseType.cs
@@ -3,7 +3,8 @@
     public enum LicenseType
     {
         /// <summary>
-        /// <para>The library is free for the commercial usage for the vast majority of users under the QuestPDF Community MIT license. Please kindly check  if you are eligible to use this license.</para>
+        /// <para>We love and highly appreciate the .NET Community and therefore the vast majority of users are welcome to use the library under the QuestPDF Community MIT License, without any limitations, even for commercial usage. Please kindly check if you are eligible to use this license.</para>
+        /// <para>License comparison: http://www.questpdf.com/pricing.html#license</para>
         /// <para>License link: https://www.questpdf.com/license-community.html</para>
         /// </summary>
         Community,
@@ -12,14 +13,16 @@
         /// <para>You must purchase the QuestPDF Professional license, if you are consuming the QuestPDF library as a Direct Package Dependency for usage in a Closed Source software in the capacity of a for-profit company/individual with more than 1M USD annual gross revenue, <c>and there are up to 10 developers</c>.</para>
         /// <para>If the number of developers is more than 10, you must purchase the QuestPDF Enterprise license.</para>
         /// <para>Before making a license purchase, please evaluate the library in a non-production environment.</para>
+        /// <para>License comparison: http://www.questpdf.com/pricing.html#license</para>
         /// <para>License link: https://www.questpdf.com/license-commercial.html</para>
         /// </summary>
         Professional,
         
         /// <summary>
         /// <para>You must purchase the QuestPDF Professional license, if you are consuming the QuestPDF library as a Direct Package Dependency for usage in a Closed Source software in the capacity of a for-profit company/individual with more than 1M USD annual gross revenue.</para>
-        /// <para>If the there are less than 10 developers, you are eligible to use the cheaper QuestPDF Professional License.</para>
+        /// <para>If there are less than 10 developers, you are eligible to use the cheaper QuestPDF Professional License.</para>
         /// <para>Before making a license purchase, please evaluate the library in a non-production environment.</para>
+        /// <para>License comparison: http://www.questpdf.com/pricing.html#license</para>
         /// <para>License link: https://www.questpdf.com/license-commercial.html</para>
         /// </summary>
         Enterprise
