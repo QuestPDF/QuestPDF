f60b2d2670fe3ad54b9c9d5f45e61b7f96fe4074
Marcin Ziąbek <marcin@ziabek.com>
2023-08-22 23:25:05 +0200

    Documentation: AspectRatio, Background, ShowEntire, EnsureSpace, PageBreak, Hyperlink, Section, SectionLink, Canvasm Documentation: AspectRatio, Background, ShowEntire, EnsureSpace, PageBreak, Hyperlink, Section, SectionLink, Canvas, Unconstrained, DefaultTextStyle, StopPaging, ScaleToFit

--- Source/QuestPDF/Fluent/ElementExtensions.cs
+++ Source/QuestPDF/Fluent/ElementExtensions.cs
@@ -42,6 +42,15 @@ namespace QuestPDF.Fluent
             return handler(parent.Container()).Container();
         }
         
+        /// <summary>
+        /// Constrains its content to maintain a given aspect ratio.
+        /// <a href="https://www.questpdf.com/api-reference/aspect-ratio.html">Documentation</a>
+        /// </summary>
+        /// <remarks>
+        /// This container enforces strict space constraints. The <see cref="DocumentLayoutException" /> may be thrown if these constraints can't be satisfied.
+        /// </remarks>
+        /// <param name="ratio">Represents the aspect ratio as a width-to-height division. For instance, a container with a width of 250 points and a height of 200 points has an aspect ratio of 1.25.</param>
+        /// <param name="option">Determines the approach the component should adopt when maintaining the specified aspect ratio.</param>
         public static IContainer AspectRatio(this IContainer element, float ratio, AspectRatioOption option = AspectRatioOption.FitWidth)
         {
             return element.Element(new AspectRatio
@@ -51,6 +60,11 @@ namespace QuestPDF.Fluent
             });
         }
 
+        /// <summary>
+        /// Sets a solid background color behind its content.
+        /// <a href="https://www.questpdf.com/api-reference/background.html">Learn more</a>
+        /// </summary>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="colorParam"]/*' />
         public static IContainer Background(this IContainer element, string color)
         {
             ColorValidator.Validate(color);
@@ -119,11 +133,33 @@ namespace QuestPDF.Fluent
             return element.Element(new SkipOnce());
         }
 
+        /// <summary>
+        /// Ensures its content is displayed entirely on a single page by disabling the default paging capability.
+        /// <a href="https://www.questpdf.com/api-reference/show-entire.html">Learn more</a>
+        /// </summary>
+        /// <remarks>
+        /// <para>While many library elements inherently support paging, allowing content to span multiple pages, this element restricts that behavior.</para>
+        /// <para>Employ this when a single-page display is crucial.</para>
+        /// <para>Be cautious: its strict space constraints can trigger the <see cref="DocumentLayoutException" /> if content exceeds the page's capacity.</para>
+        /// </remarks>
         public static IContainer ShowEntire(this IContainer element)
         {
             return element.Element(new ShowEntire());
         }
 
+        /// <summary>
+        /// <para>Serves as a less-strict approach compared to the <see cref="ElementExtensions.ShowEntire">ShowEntire</see> element.</para>
+        /// <para>
+        /// It impacts only the very first page of its content's occurence.
+        /// If the element fits within its first page, it's rendered as usual.
+        /// However, if the element doesn't fit and the available space has less vertical space than required height, the content is entirely shifted to the next page.
+        /// </para>
+        /// <br />
+        /// <a href="https://www.questpdf.com/api-reference/ensure-space.html">Learn more</a>
+        /// </summary>
+        /// <remarks>
+        /// This is especially useful for elements like tables, where you'd want to display several rows together. By setting the minHeight, you can avoid scenarios where only a single row appears at the page's end, ensuring a more cohesive presentation.
+        /// </remarks>
         public static IContainer EnsureSpace(this IContainer element, float minHeight = Elements.EnsureSpace.DefaultMinHeight)
         {
             return element.Element(new EnsureSpace
@@ -132,6 +168,10 @@ namespace QuestPDF.Fluent
             });
         }
 
+        /// <summary>
+        /// Inserts a break that pushes the inner content to start on a new page.
+        /// <a href="https://www.questpdf.com/api-reference/page-break.html">Learn more</a>
+        /// </summary>
         public static void PageBreak(this IContainer element)
         {
             element.Element(new PageBreak());
@@ -148,6 +188,11 @@ namespace QuestPDF.Fluent
             return element.Hyperlink(url);
         }
         
+        /// <summary>
+        /// Creates a clickable area that redirects the user to a designated webpage.
+        /// <a href="https://www.questpdf.com/api-reference/hyperlink.html">Learn more</a>
+        /// </summary>
+        /// <param name="url">The URL of the webpage to which the user will be redirected.</param>
         public static IContainer Hyperlink(this IContainer element, string url)
         {
             return element.Element(new Hyperlink
@@ -162,6 +207,16 @@ namespace QuestPDF.Fluent
             return element.Section(locationName);
         }
         
+        /// <summary>
+        /// Defines a named fragment of the document that can span multiple pages.
+        /// <a href="https://www.questpdf.com/api-reference/section.html">Learn more</a>
+        /// </summary>
+        /// <remarks>
+        /// <para>Several other elements interact with sections:</para>
+        /// <para>Use <see cref="ElementExtensions.SectionLink">SectionLink</see> to create a clickable area redirecting user to the first page of the associated section.</para>
+        /// <para>The <see cref="TextExtensions.Text(IContainer, Action&lt;TextDescriptor&gt;)">Text</see> element can display section properties, such as the starting page, ending page, and length.</para>
+        /// </remarks>
+        /// <param name="sectionName">An internal text key representing the section. It should be unique and won't appear in the final document.</param>
         public static IContainer Section(this IContainer element, string sectionName)
         {
             return element.Element(new Section
@@ -176,6 +231,11 @@ namespace QuestPDF.Fluent
             return element.SectionLink(locationName);
         }
         
+        /// <summary>
+        /// Creates a clickable area that navigates the user to a designated section.
+        /// <a href="https://www.questpdf.com/api-reference/section-link.html">Learn more</a>
+        /// </summary>
+        /// <param name="sectionName">The text key corresponding to the value used when defining the <see cref="ElementExtensions.Section">Section</see> element.</param>
         public static IContainer SectionLink(this IContainer element, string sectionName)
         {
             return element.Element(new SectionLink
@@ -194,6 +254,14 @@ namespace QuestPDF.Fluent
             return condition ? element : new Container();
         }
         
+        /// <summary>
+        /// Provides direct access to the low-level SkiaSharp API.
+        /// <a href="https://www.questpdf.com/api-reference/canvas.html">Documentation</a>
+        /// </summary>
+        /// <example>
+        /// <para>Use this element when needing to render advanced shapes not directly available in the QuestPDF API.</para>
+        /// <para>It's also ideal for integrating with other SkiaSharp-based libraries, such as charting tools, to produce pixel-perfect vector graphics.</para>
+        /// </example>
         public static void Canvas(this IContainer element, DrawOnCanvas handler)
         {
             element.Element(new Canvas
@@ -208,16 +276,33 @@ namespace QuestPDF.Fluent
             return element.Element(new MinimalBox());
         }
         
+        /// <summary>
+        /// Renders its content in the most compact size achievable. 
+        /// Ideal for situations where the parent element provides more than necessary.
+        /// <br />
+        /// <a href="https://www.questpdf.com/api-reference/minimal-box.html">Learn more</a>
+        /// </summary>
         public static IContainer MinimalBox(this IContainer element)
         {
             return element.Element(new MinimalBox());
         }
         
+        /// <summary>
+        /// Removes size constraints and grants its content virtually unlimited space.
+        /// <a href="https://www.questpdf.com/api-reference/unconstrained.html">Learn more</a>
+        /// </summary>
         public static IContainer Unconstrained(this IContainer element)
         {
             return element.Element(new Unconstrained());
         }
         
+        /// <summary>
+        /// Applies a default text style to all nested <see cref="TextExtensions.Text">Text</see> elements.
+        /// </summary>
+        /// <remarks>
+        /// If multiple text elements have a similar style, using this element can help simplify your code.
+        /// </remarks>
+        /// <param name="textStyle">A TextStyle object used to override specific properties.</param>
         public static IContainer DefaultTextStyle(this IContainer element, TextStyle textStyle)
         {
             return element.Element(new DefaultTextStyle
@@ -226,6 +311,13 @@ namespace QuestPDF.Fluent
             });
         }
         
+        /// <summary>
+        /// Applies a default text style to all nested <see cref="TextExtensions.Text">Text</see> elements.
+        /// </summary>
+        /// <remarks>
+        /// If multiple text elements have a similar style, using this element can help simplify your code.
+        /// </remarks>
+        /// <param name="handler">A handler to modify the default text style.</param>
         public static IContainer DefaultTextStyle(this IContainer element, Func<TextStyle, TextStyle> handler)
         {
             return element.Element(new DefaultTextStyle
@@ -234,11 +326,23 @@ namespace QuestPDF.Fluent
             });
         }
         
+        /// <summary>
+        /// Renders the element exclusively on the first page. Any portion of the element that doesn't fit is omitted.
+        /// <a href="https://www.questpdf.com/api-reference/stop-paging.html">Learn more</a>
+        /// </summary>
         public static IContainer StopPaging(this IContainer element)
         {
             return element.Element(new StopPaging());
         }
         
+        /// <summary>
+        /// Adjusts its content to fit within the available space by scaling it down proportionally if necessary.
+        /// <a href="https://www.questpdf.com/api-reference/scale-to-fit.html">Learn more</a>
+        /// </summary>
+        /// <remarks>
+        /// <para>This container determines the best scale value through multiple scaling operations. With complex content, this may impact performance.</para>
+        /// <para>Pairing with certain elements, such as <see cref="ElementExtensions.AspectRatio">AspectRatio</see>, might still lead to a <see cref="DocumentLayoutException" />.</para>
+        /// </remarks>
         public static IContainer ScaleToFit(this IContainer element)
         {
             return element.Element(new ScaleToFit());
