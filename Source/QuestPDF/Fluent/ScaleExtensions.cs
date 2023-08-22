438c16aa7f470e3e02469a2835359aa16e4e8b09
Marcin Ziąbek <marcin@ziabek.com>
2023-08-22 15:46:38 +0200

    Documentation: Scale

--- Source/QuestPDF/Fluent/ScaleExtensions.cs
+++ Source/QuestPDF/Fluent/ScaleExtensions.cs
@@ -14,31 +14,70 @@ namespace QuestPDF.Fluent
             return element.Element(scale);
         }
         
-        public static IContainer Scale(this IContainer element, float value)
+        /// <summary>
+        /// Scales its inner content proportionally.
+        /// <a href="https://www.questpdf.com/api-reference/scale.html">Learn more</a>
+        /// </summary>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.remarks"]/*' />
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.factorParam"]/*' />
+        public static IContainer Scale(this IContainer element, float factor)
         {
-            return element.ScaleHorizontal(value).ScaleVertical(value);
+            return element.ScaleHorizontal(factor).ScaleVertical(factor);
         }
         
-        public static IContainer ScaleHorizontal(this IContainer element, float value)
+        /// <summary>
+        /// Scales the available horizontal space (along the X axis), causing content to appear expanded or squished, rather than simply larger or smaller.
+        /// <a href="https://www.questpdf.com/api-reference/scale.html">Learn more</a>
+        /// </summary>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.remarks"]/*' />
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.factorParam"]/*' />
+        public static IContainer ScaleHorizontal(this IContainer element, float factor)
         {
-            return element.Scale(x => x.ScaleX *= value);
+            return element.Scale(x => x.ScaleX *= factor);
         }
         
+        /// <summary>
+        /// Scales the available vertical space (along the Y axis), causing content to appear expanded or squished, rather than simply larger or smaller.
+        /// <a href="https://www.questpdf.com/api-reference/scale.html">Learn more</a> 
+        /// </summary>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.remarks"]/*' />
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="scale.factorParam"]/*' />
+        public static IContainer ScaleVertical(this IContainer element, float factor)
+        {
+            return element.Scale(x => x.ScaleY *= factor);
+        }
+        
+        /// <summary>
+        /// Flips its content to create a mirror image along the Y axis, swapping elements from left to right.
+        /// <a href="https://www.questpdf.com/api-reference/flip.html">Learn more</a>
+        /// </summary>
+        /// <example>
+        /// Elements on the left will appear on the right.
+        /// </example>
         public static IContainer FlipHorizontal(this IContainer element)
         {
             return element.ScaleHorizontal(-1);
         }
         
-        public static IContainer ScaleVertical(this IContainer element, float value)
-        {
-            return element.Scale(x => x.ScaleY *= value);
-        }
-        
+        /// <summary>
+        /// Flips its content to create a mirror image along the X axis, moving elements from the top to the bottom.
+        /// <a href="https://www.questpdf.com/api-reference/flip.html">Learn more</a>
+        /// </summary>
+        /// <example>
+        /// Elements at the top will be positioned at the bottom.
+        /// </example>
         public static IContainer FlipVertical(this IContainer element)
         {
             return element.ScaleVertical(-1);
         }
         
+        /// <summary>
+        /// Creates a mirror image of its content across both axes.
+        /// <a href="https://www.questpdf.com/api-reference/flip.html">Learn more</a>
+        /// </summary>
+        /// <example>
+        /// Elements originally in the top-left corner will be positioned in the bottom-right corner.
+        /// </example>
         public static IContainer FlipOver(this IContainer element)
         {
             return element.FlipHorizontal().FlipVertical();
