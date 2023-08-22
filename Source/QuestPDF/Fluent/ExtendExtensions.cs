dbdf0e94731e6e749ef0586149da0ee48cf6ffe9
Marcin Ziąbek <marcin@ziabek.com>
2023-08-22 22:41:57 +0200

    Documentation: Extend

--- Source/QuestPDF/Fluent/ExtendExtensions.cs
+++ Source/QuestPDF/Fluent/ExtendExtensions.cs
@@ -14,16 +14,28 @@ namespace QuestPDF.Fluent
             return element.Element(extend);
         }
         
+        /// <summary>
+        /// Forces its content to occupy the full available space, maximizing both width and height.
+        /// <a href="https://www.questpdf.com/api-reference/extend.html">Learn more</a>
+        /// </summary>
         public static IContainer Extend(this IContainer element)
         {
             return element.ExtendVertical().ExtendHorizontal();
         }
         
+        /// <summary>
+        /// Forces its content to occupy the full available vertical space, maximizing height usage.
+        /// <a href="https://www.questpdf.com/api-reference/extend.html">Learn more</a>
+        /// </summary>
         public static IContainer ExtendVertical(this IContainer element)
         {
             return element.Extend(x => x.ExtendVertical = true);
         }
         
+        /// <summary>
+        /// Expands its content to occupy the full available horizontal space, maximizing width usage.
+        /// <a href="https://www.questpdf.com/api-reference/extend.html">Learn more</a>
+        /// </summary>
         public static IContainer ExtendHorizontal(this IContainer element)
         {
             return element.Extend(x => x.ExtendHorizontal = true);
