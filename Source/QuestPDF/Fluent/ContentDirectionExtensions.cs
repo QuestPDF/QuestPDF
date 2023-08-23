24297c1dfdb58cb7044a986998b9cc5ce3d726e3
Marcin Ziąbek <marcin@ziabek.com>
2023-08-23 17:34:24 +0200

    Improvement: Content Direction

--- Source/QuestPDF/Fluent/ContentDirectionExtensions.cs
+++ Source/QuestPDF/Fluent/ContentDirectionExtensions.cs
@@ -17,11 +17,7 @@ namespace QuestPDF.Fluent
         /// Sets the left-to-right (LTR) direction for its entire content.
         /// <a href="https://www.questpdf.com/api-reference/content-direction.html">Learn more</a>
         /// </summary>
-        /// <remarks>
-        /// The content direction affects various layout structures. In LTR mode, items are typically aligned to the left. 
-        /// This mode also influences the direction of items in certain layouts. For instance, in a row element with LTR mode, 
-        /// the first item is positioned on the left, while the last item is on the right.
-        /// </remarks>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="contentDirection.ltr.remarks"]/*' />
         public static IContainer ContentFromLeftToRight(this IContainer element)
         {
             return element.ContentDirection(Infrastructure.ContentDirection.LeftToRight);
@@ -31,11 +27,7 @@ namespace QuestPDF.Fluent
         /// Sets the left-to-right (LTR) direction for its entire content.
         /// <a href="https://www.questpdf.com/api-reference/content-direction.html">Learn more</a>
         /// </summary>
-        /// <remarks>
-        /// The content direction affects various layout structures. In RTL mode, items are typically aligned to the right. 
-        /// This mode also influences the direction of items in certain layouts. For instance, in a row element with RTL mode, 
-        /// the first item is positioned on the right, while the last item is on the left.
-        /// </remarks>
+        /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="contentDirection.rtl.remarks"]/*' />
         public static IContainer ContentFromRightToLeft(this IContainer element)
         {
             return element.ContentDirection(Infrastructure.ContentDirection.RightToLeft);
