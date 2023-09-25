c24b56a04d3daf70faca752d2af7e64657ecf18c
Marcin Ziąbek <marcin@ziabek.com>
2023-09-25 21:28:06 +0200

    2023.9.0 Release

--- Source/QuestPDF/Previewer/PreviewerService.cs
+++ Source/QuestPDF/Previewer/PreviewerService.cs
@@ -19,7 +19,7 @@ namespace QuestPDF.Previewer
         public  event Action? OnPreviewerStopped;
 
         private const int RequiredPreviewerVersionMajor = 2023;
-        private const int RequiredPreviewerVersionMinor = 6;
+        private const int RequiredPreviewerVersionMinor = 9;
         
         public PreviewerService(int port)
         {
