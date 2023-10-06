3449ce0ceb44979137414662c845b9a9bee92a3e
Marcin Ziąbek <marcin@ziabek.com>
2023-10-06 23:34:46 +0200

    Fixed build

--- Source/QuestPDF/Infrastructure/FontMetrics.cs
+++ Source/QuestPDF/Infrastructure/FontMetrics.cs
@@ -1,13 +1,14 @@
-namespace QuestPDF.Infrastructure;
-
-internal struct FontMetrics
+namespace QuestPDF.Infrastructure
 {
-    public float Ascent { get; set; }
-    public float Descent { get; set; }
+    internal struct FontMetrics
+    {
+        public float Ascent { get; set; }
+        public float Descent { get; set; }
     
-    public float UnderlinePosition { get; set; }
-    public float UnderlineThickness { get; set; }
+        public float UnderlinePosition { get; set; }
+        public float UnderlineThickness { get; set; }
     
-    public float StrikeoutPosition { get; set; }
-    public float StrikeoutThickness { get; set; }
+        public float StrikeoutPosition { get; set; }
+        public float StrikeoutThickness { get; set; }
+    }
 }
