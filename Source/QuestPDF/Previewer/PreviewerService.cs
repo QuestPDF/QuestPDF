e27b3d1f14caabf68cdd961ec17dbd5392303ca3
Marcin Ziąbek <marcin@ziabek.com>
2023-10-13 03:00:50 +0200

    2023.10.0-alpha0 Improved developer experience during debugging of document layout issues

--- Source/QuestPDF/Previewer/PreviewerService.cs
+++ Source/QuestPDF/Previewer/PreviewerService.cs
@@ -19,7 +19,7 @@ namespace QuestPDF.Previewer
         public  event Action? OnPreviewerStopped;
 
         private const int RequiredPreviewerVersionMajor = 2023;
-        private const int RequiredPreviewerVersionMinor = 9;
+        private const int RequiredPreviewerVersionMinor = 10;
         
         public PreviewerService(int port)
         {
