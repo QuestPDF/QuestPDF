364db80dedf8d23cc8f2554cd26965e87f975dca
Marcin Ziąbek <marcin@ziabek.com>
2023-11-13 21:48:35 +0100

    Disabled warning CS1591

--- Source/QuestPDF/Infrastructure/FontWeight.cs
+++ Source/QuestPDF/Infrastructure/FontWeight.cs
@@ -1,4 +1,6 @@
-﻿namespace QuestPDF.Infrastructure
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace QuestPDF.Infrastructure
 {
     public enum FontWeight
     {
