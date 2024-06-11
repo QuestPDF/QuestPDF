8797f34eed5621e300546c2a5877fafc8db76991
Marcin Ziąbek <marcin@ziabek.com>
2024-06-11 11:09:20 +0200

    Fixed API visibility

--- Source/QuestPDF/Skia/Text/SkTypeface.cs
+++ Source/QuestPDF/Skia/Text/SkTypeface.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace QuestPDF.Skia.Text;
 
-public sealed class SkTypeface : IDisposable
+internal sealed class SkTypeface : IDisposable
 {
     public IntPtr Instance { get; private set; }
 
