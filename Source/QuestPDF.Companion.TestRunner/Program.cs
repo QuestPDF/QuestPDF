a106fa5041ec4c2ef1fa9987d36729dae36b2a7b
Marcin Ziąbek <marcin@ziabek.com>
2024-08-26 12:34:55 +0200

    Added sample Companion project

---
+++ Source/QuestPDF.Companion.TestRunner/Program.cs
@@ -0,0 +1,39 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+
+await Document
+    .Create(container =>
+    {
+        container.Page(page =>
+        {
+            page.Size(PageSizes.A4);
+            page.Margin(2, Unit.Centimetre);
+            page.PageColor(Colors.White);
+            page.DefaultTextStyle(x => x.FontSize(20));
+        
+            page.Header()
+                .Text("Hello PDF!")
+                .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+        
+            page.Content()
+                .PaddingVertical(1, Unit.Centimetre)
+                .Column(x =>
+                {
+                    x.Spacing(20);
+                
+                    x.Item().Text(Placeholders.LoremIpsum());
+                    x.Item().Image(Placeholders.Image(200, 100));
+                });
+        
+            page.Footer()
+                .AlignCenter()
+                .Text(x =>
+                {
+                    x.Span("Page ");
+                    x.CurrentPageNumber();
+                });
+        });
+    })
+    .ShowInPreviewerAsync();
