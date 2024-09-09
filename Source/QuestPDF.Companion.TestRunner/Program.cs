f0257663d5d3314b03880b9234cbb6ce506e57f7
Marcin Ziąbek <marcin@ziabek.com>
2024-09-09 15:04:47 +0200

    Companion: added layout error test

--- Source/QuestPDF.Companion.TestRunner/Program.cs
+++ Source/QuestPDF.Companion.TestRunner/Program.cs
@@ -8,9 +8,10 @@ using QuestPDF.ReportSample.Layouts;
 
 Settings.License = LicenseType.Professional;
 
-await RunGenericException();
-await RunSimpleDocument();
-await RunReportDocument();
+//await RunGenericException();
+await RunLayoutError();
+//await RunSimpleDocument();
+//await RunReportDocument();
 
 Task RunGenericException()
 {
@@ -24,11 +25,41 @@ Task RunGenericException()
                     .Column(x =>
                     {
                         x.Spacing(20);
-                
+
                         x.Item().Text(Placeholders.LoremIpsum());
                         x.Item().Hyperlink("questpdf.com").Image(Placeholders.Image(200, 100));
 
                         throw new Exception("Test exception");
+                    });
+            });
+        })
+        .ShowInCompanionAsync();
+}
+
+Task RunLayoutError()
+{
+    return Document
+        .Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(2, Unit.Centimetre);
+                page.PageColor(Colors.White);
+                page.DefaultTextStyle(x => x.FontSize(20));
+
+                page.Content()
+                    .PaddingVertical(1, Unit.Centimetre)
+                    .Column(x =>
+                    {
+                        x.Spacing(20);
+                    
+                        x.Item().Text(Placeholders.LoremIpsum());
+                    
+                        foreach (var i in Enumerable.Range(0, 15))
+                        {
+                            x.Item().Background(Colors.Grey.Lighten3).MaxWidth(200).Container().Width(100 + i * 10).Height(50).Text($"Item {i}");
+                        }
                     });
             });
         })
