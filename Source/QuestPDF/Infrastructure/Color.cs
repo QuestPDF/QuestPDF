66aa03183e797a665a84fd55fb45ac193b92a640
Marcin Ziąbek <marcin@ziabek.com>
2024-10-14 19:49:59 +0200

    Fixed parsing color value from string and uint

--- Source/QuestPDF/Infrastructure/Color.cs
+++ Source/QuestPDF/Infrastructure/Color.cs
@@ -11,30 +11,53 @@ public readonly struct Color
     public byte Green => (byte) ((Hex & 0x00FF00) >> 8);
     public byte Blue => (byte) (Hex & 0x0000FF);
 
+    /// <summary>
+    /// Creates a new color from a hex value following the ARGB format.
+    /// For example 0xFF0000FF represents a fully opaque blue color.
+    /// </summary>
     public Color(uint hex)
     {
-        if (hex <= 0x00FFFFFF && hex != 0x00000000) 
-            hex |= 0xFF000000;
-        
         Hex = hex;
     }
-    
+
+
+    /// <summary>
+    /// Creates a new color instance with the specified alpha transparency value.
+    /// The alpha value should be within the range 0 to 255, where 0 represents fully transparent
+    /// and 255 represents fully opaque.
+    /// </summary>
+    /// <returns>A new color instance with the adjusted alpha transparency.</returns>
     public Color WithAlpha(byte alpha)
     {
         var newHex = (Hex & 0x00FFFFFF) | ((uint)alpha << 24);
         return new Color(newHex);
     }
-    
+
+    /// <summary>
+    /// Creates a new color instance from a hex string representation.
+    /// The string should follow the formats: #RGB, #ARGB, #RRGGBB, or #AARRGGBB.
+    /// The hash sign is optional; for example: RGB, ARGB, RRGGBB, or AARRGGBB are also valid.
+    /// </summary>
+    /// <param name="hex">The hex string representing the color.</param>
+    /// <returns>A new color instance created from the hex string.</returns>
     public static Color FromHex(string hex)
     {
         return ColorParser.ParseColorHex(hex);
     }
 
+    /// <summary>
+    /// Creates a new color instance given the red, green, and blue component values.
+    /// Each component should be within the range 0 to 255.
+    /// </summary>
     public static Color FromRGB(byte red, byte green, byte blue)
     {
         return FromARGB(255, red, green, blue);
     }
-    
+
+    /// <summary>
+    /// Creates a new color instance from the specified alpha, red, green, and blue component values.
+    /// Each component should be within the range 0 to 255.
+    /// </summary>
     public static Color FromARGB(byte alpha, byte red, byte green, byte blue)
     {
         return new Color((uint) (alpha << 24 | red << 16 | green << 8 | blue));
@@ -57,6 +80,9 @@ public readonly struct Color
     
     public static implicit operator Color(uint hex)
     {
+        if (hex <= 0x00FFFFFF) 
+            hex |= 0xFF000000;
+        
         return new Color(hex);
     }
 
