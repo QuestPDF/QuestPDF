3c19b8d488bc8ee84784c0908fe2bda3840b6cf7
Marcin Ziąbek <marcin@ziabek.com>
2025-02-19 16:11:10 +0100

    Improved in-code documentation for DocumentMetadata

--- Source/QuestPDF/Infrastructure/DocumentMetadata.cs
+++ Source/QuestPDF/Infrastructure/DocumentMetadata.cs
@@ -4,14 +4,47 @@ namespace QuestPDF.Infrastructure
 {
     public class DocumentMetadata
     {
+        /// <summary>
+        /// Represents the main heading or name of the document, often displayed as a prominent identifier or label in PDF metadata.
+        /// </summary>
         public string? Title { get; set; }
+
+        /// <summary>
+        /// Specifies the individual or entity responsible for creating the document.
+        /// </summary>
         public string? Author { get; set; }
+
+        /// <summary>
+        /// Provides a brief description or main topic related to the document content.
+        /// </summary>
         public string? Subject { get; set; }
+
+        /// <summary>
+        /// Defines a collection of terms or phrases that describe the document's content or purpose.
+        /// Improves categorization and searchability of the document.
+        /// </summary>
         public string? Keywords { get; set; }
+
+        /// <summary>
+        /// Identifies the software or system that generated the document.
+        /// </summary>
         public string? Creator { get; set; }
+
+        /// <summary>
+        /// Specifies the name of the application or library that generated the document.
+        /// </summary>
         public string? Producer { get; set; }
 
+        /// <summary>
+        /// Represents the date and time when the document was created.
+        /// This property is used to specify the creation timestamp.
+        /// </summary>
         public DateTimeOffset CreationDate { get; set; } = DateTimeOffset.Now;
+
+        /// <summary>
+        /// Stores the most recent date and time when the content or metadata of the document was updated.
+        /// It is used to provide metadata information about the last revision of the document.
+        /// </summary>
         public DateTimeOffset ModifiedDate { get; set; } = DateTimeOffset.Now;
 
         public static DocumentMetadata Default => new DocumentMetadata();
