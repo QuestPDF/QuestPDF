d3a2f69f70951c54280faf60faff3f96c316b2a4
Marcin Ziąbek <marcin@ziabek.com>
2025-06-19 19:22:41 +0200

    Add fluent API method for the line element (gradient and dashed pattern)

--- Source/QuestPDF/Fluent/LineExtensions.cs
+++ Source/QuestPDF/Fluent/LineExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using QuestPDF.Elements;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
@@ -52,9 +53,45 @@ namespace QuestPDF.Fluent
         /// Specifies the color for the line.
         /// </summary>
         /// <include file='../Resources/Documentation.xml' path='documentation/doc[@for="colorParam"]/*' />
-        public static void LineColor(this ILine descriptor, Color color)
+        public static ILine LineColor(this ILine descriptor, Color color)
         {
             (descriptor as Line).Color = color;
+            return descriptor;
+        }
+
+        /// <summary>
+        /// Configures a dashed pattern for the line.
+        /// For example, a pattern of [2, 3] creates a dash of 2 units followed by a gap of 3 units.
+        /// </summary>
+        /// <param name="dashPattern">The length of this array must be even.</param>
+        public static ILine LineDashPattern(this ILine descriptor, float[] dashPattern, Unit unit = Unit.Point)
+        {
+            if (dashPattern == null)
+                throw new ArgumentNullException(nameof(dashPattern), "The dash pattern cannot be null.");
+            
+            if (dashPattern.Length == 0)
+                throw new ArgumentException("The dash pattern cannot be empty.", nameof(dashPattern));
+            
+            if (dashPattern.Length % 2 != 0)
+                throw new ArgumentException("The dash pattern must contain an even number of elements.", nameof(dashPattern));
+            
+            (descriptor as Line).DashPattern = dashPattern.Select(x => x.ToPoints(unit)).ToArray();
+            return descriptor;
+        }
+
+        /// <summary>
+        /// Applies a linear gradient to a line using the specified colors.
+        /// </summary>
+        public static ILine LineGradient(this ILine descriptor, params Color[] colors)
+        {
+            if (colors == null)
+                throw new ArgumentNullException(nameof(colors), "The gradient colors cannot be null.");
+
+            if (colors.Length == 0)
+                throw new ArgumentException("The gradient colors cannot be empty.", nameof(colors));
+
+            (descriptor as Line).GradientColors = colors;
+            return descriptor;
         }
     }
 }
