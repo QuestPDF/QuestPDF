7cb8a86279dda3d48736113d6004afb23b6bc2cc
Marcin Ziąbek <marcin@ziabek.com>
2025-06-25 19:18:06 +0200

    Disabled visual tests on runtimes different from osx-arm64

--- Source/QuestPDF.VisualTests/TestsSetup.cs
+++ Source/QuestPDF.VisualTests/TestsSetup.cs
@@ -1,4 +1,5 @@
-﻿using QuestPDF.Infrastructure;
+﻿using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 
 namespace QuestPDF.VisualTests
 {
@@ -8,8 +9,22 @@ namespace QuestPDF.VisualTests
         [OneTimeSetUp]
         public static void Setup()
         {
-            VisualTestEngine.ClearActualOutputDirectories();
+            var currentRuntime = NativeDependencyProvider.GetRuntimePlatform();
+
+            if (currentRuntime != "osx-arm64")
+            {
+                Assert.Ignore(
+                    "Visual tests are performed based on osx-arm64 runtime output. " +
+                    "Each operating system (Windows, Linux, macOS) uses different font analysis and text rendering libraries. " +
+                    "Moreover, each CPU architecture may produce slightly different floating-point calculation results. " +
+                    "This may lead to different results even though the same code and configuration are used. " +
+                    "Therefore, visual tests are not expected to pass entirely on other operating systems or CPU architectures.");
+            }
+                
             QuestPDF.Settings.License = LicenseType.Community;
+            QuestPDF.Settings.UseEnvironmentFonts = false;
+            
+            VisualTestEngine.ClearActualOutputDirectories();
         }
     }
 }
