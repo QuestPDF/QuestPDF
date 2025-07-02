4c9d7ca465c37c37eb56e22ab21ed963576d757c
Marcin Ziąbek <marcin@ziabek.com>
2025-07-02 08:32:31 +0200

    Change the icon and its color in the Placeholder element

--- Source/QuestPDF/Elements/Placeholder.cs
+++ Source/QuestPDF/Elements/Placeholder.cs
@@ -11,7 +11,7 @@ namespace QuestPDF.Elements
         public void Compose(IContainer container)
         {
             const float imageSvgSize = 24f;
-            const string imageSvgPath = "M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z";
+            const string imageSvgPath = "M19,19H5V5H19M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M13.96,12.29L11.21,15.83L9.25,13.47L6.5,17H17.5L13.96,12.29Z";
             
             container
                 .Background(Colors.Grey.Lighten2)
@@ -21,7 +21,7 @@ namespace QuestPDF.Elements
                 .Element(x =>
                 {
                     if (string.IsNullOrWhiteSpace(Text))
-                        x.Height(imageSvgSize).Width(imageSvgSize).SvgPath(imageSvgPath, Colors.White);
+                        x.Height(imageSvgSize).Width(imageSvgSize).SvgPath(imageSvgPath, Colors.Black);
                     
                     else
                         x.Text(Text).FontSize(14);
