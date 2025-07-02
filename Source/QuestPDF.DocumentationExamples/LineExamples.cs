e023c5d31f57c9979ed12697006c4656b16579ae
Marcin Ziąbek <marcin@ziabek.com>
2025-07-02 08:34:36 +0200

    Enhance documentation examples for the Line element

--- Source/QuestPDF.DocumentationExamples/LineExamples.cs
+++ Source/QuestPDF.DocumentationExamples/LineExamples.cs
@@ -18,6 +18,7 @@ public class LineExamples
                     page.MaxSize(new PageSize(1000, 1000));
                     page.DefaultTextStyle(x => x.FontSize(20));
                     page.Margin(25);
+                    page.PageColor(Colors.White);
 
                     page.Content()
                         .Row(row =>
@@ -48,6 +49,7 @@ public class LineExamples
                     page.MaxSize(new PageSize(1000, 1000));
                     page.DefaultTextStyle(x => x.FontSize(20));
                     page.Margin(25);
+                    page.PageColor(Colors.White);
 
                     page.Content()
                         .Column(column =>
@@ -64,5 +66,177 @@ public class LineExamples
                 });
             })
             .GenerateImages(x => "line-horizontal.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
+    }
+    
+    [Test]
+    public void Thickness()
+    {
+        Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.MinSize(new PageSize(0, 0));
+                    page.MaxSize(new PageSize(1000, 1000));
+                    page.DefaultTextStyle(x => x.FontSize(20));
+                    page.Margin(25);
+                    page.PageColor(Colors.White);
+
+                    page.Content()
+                        .Column(column =>
+                        {
+                            column.Spacing(20);
+    
+                            foreach (var thickness in new[] { 1, 2, 4, 8 })
+                            {
+                                column.Item()
+                                    .Width(200)
+                                    .LineHorizontal(thickness);
+                            }
+                        });
+                });
+            })
+            .GenerateImages(x => "line-thickness.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
+    }
+
+    [Test]
+    public void SolidColor()
+    {
+        Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.MinSize(new PageSize(0, 0));
+                    page.MaxSize(new PageSize(1000, 1000));
+                    page.DefaultTextStyle(x => x.FontSize(20));
+                    page.Margin(25);
+                    page.PageColor(Colors.White);
+
+                    page.Content()
+                        .Column(column =>
+                        {
+                            var colors = new[]
+                            {
+                                Colors.Red.Medium,
+                                Colors.Green.Medium,
+                                Colors.Blue.Medium,
+                            };
+                            
+                            column.Spacing(20);
+                
+                            foreach (var color in colors)
+                            {
+                                column.Item()
+                                    .Width(200)
+                                    .LineHorizontal(5)
+                                    .LineColor(color);
+                            }
+                        });
+                });
+            })
+            .GenerateImages(x => "line-color-solid.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
+    }
+    
+    [Test]
+    public void Gradient()
+    {
+        Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.MinSize(new PageSize(0, 0));
+                    page.MaxSize(new PageSize(1000, 1000));
+                    page.DefaultTextStyle(x => x.FontSize(20));
+                    page.Margin(25);
+                    page.PageColor(Colors.White);
+
+                    page.Content()
+                        .Column(column =>
+                        {
+                            column.Spacing(20);
+
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineGradient([Colors.Red.Medium, Colors.Orange.Medium]);
+                
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineGradient([Colors.Orange.Medium, Colors.Yellow.Medium, Colors.Lime.Medium]);
+                
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineGradient([Colors.Blue.Lighten2, Colors.LightBlue.Lighten1, Colors.Cyan.Medium, Colors.Teal.Darken1, Colors.Green.Darken2]);
+                        });
+                });
+            })
+            .GenerateImages(x => "line-color-gradient.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
+    }
+    
+    [Test]
+    public void DashPattern()
+    {
+        Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.MinSize(new PageSize(0, 0));
+                    page.MaxSize(new PageSize(1000, 1000));
+                    page.DefaultTextStyle(x => x.FontSize(20));
+                    page.Margin(25);
+                    page.PageColor(Colors.White);
+
+                    page.Content()
+                        .Column(column =>
+                        {
+                            column.Spacing(20);
+
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineDashPattern([4f, 4f]);
+                
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineDashPattern([12f, 12f]);
+                
+                            column.Item()
+                                .Width(200)
+                                .LineHorizontal(5)
+                                .LineDashPattern([4f, 4f, 12f, 4f]);
+                        });
+                });
+            })
+            .GenerateImages(x => "line-dash-pattern.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
+    }
+    
+    [Test]
+    public void Complex()
+    {
+        Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.MinSize(new PageSize(0, 0));
+                    page.MaxSize(new PageSize(1000, 1000));
+                    page.DefaultTextStyle(x => x.FontSize(20));
+                    page.Margin(25);
+                    page.PageColor(Colors.White);
+
+                    page.Content()
+                        .Width(300)
+                        .LineHorizontal(8)
+                        .LineDashPattern([4, 4, 8, 8, 12, 12])
+                        .LineGradient([Colors.Red.Medium, Colors.Orange.Medium, Colors.Yellow.Medium]);
+                });
+            })
+            .GenerateImages(x => "line-example.webp", new ImageGenerationSettings() { ImageFormat = ImageFormat.Webp, ImageCompressionQuality = ImageCompressionQuality.Best, RasterDpi = 144 });
     }
 }
