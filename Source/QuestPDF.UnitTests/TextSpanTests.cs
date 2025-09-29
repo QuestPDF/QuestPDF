d3f75075c1502f4a7b3a9f304628be821040638f
Marcin Ziąbek <marcin@ziabek.com>
2025-09-30 00:08:00 +0200

    Add tests for font feature enabling and disabling in TextStyle

--- Source/QuestPDF.UnitTests/TextSpanTests.cs
+++ Source/QuestPDF.UnitTests/TextSpanTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using QuestPDF.Elements.Text;
 using QuestPDF.Elements.Text.Items;
 using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 namespace QuestPDF.UnitTests;
@@ -129,6 +130,45 @@ public class TextSpanTests
         var (descriptor, textBlockSpan) = CreateTextBlockSpan();
         descriptor.Superscript();
         Assert.That(textBlockSpan.Style.FontPosition, Is.EqualTo(FontPosition.Superscript));
+    }
+    
+    #endregion
+    
+    #region Font Features
+    
+    [Test]
+    public void EnableFontFeature_SingleFeature()
+    {
+        var (descriptor, textBlockSpan) = CreateTextBlockSpan();
+        
+        descriptor.EnableFontFeature(FontFeatures.StandardLigatures);
+        Assert.That(textBlockSpan.Style.FontFeatures, Has.Length.EqualTo(1));
+        Assert.That(textBlockSpan.Style.FontFeatures[0], Is.EqualTo((FontFeatures.StandardLigatures, true)));
+    }
+    
+    [Test]
+    public void DisableFontFeature_SingleFeature()
+    {
+        var (descriptor, textBlockSpan) = CreateTextBlockSpan();
+        
+        descriptor.DisableFontFeature(FontFeatures.Kerning);
+        Assert.That(textBlockSpan.Style.FontFeatures, Has.Length.EqualTo(1));
+        Assert.That(textBlockSpan.Style.FontFeatures[0], Is.EqualTo((FontFeatures.Kerning, false)));
+    }
+
+    [Test]
+    public void FontFeatures_MixedEnableDisable()
+    {
+        var textStyle = TextStyle.Default
+            .EnableFontFeature(FontFeatures.StandardLigatures)
+            .DisableFontFeature(FontFeatures.Kerning)
+            .DisableFontFeature(FontFeatures.DiscretionaryLigatures)
+            .EnableFontFeature(FontFeatures.Kerning);
+        
+        Assert.That(textStyle.FontFeatures, Has.Length.EqualTo(3));
+        Assert.That(textStyle.FontFeatures[0], Is.EqualTo((FontFeatures.Kerning, true)));
+        Assert.That(textStyle.FontFeatures[1], Is.EqualTo((FontFeatures.DiscretionaryLigatures, false)));
+        Assert.That(textStyle.FontFeatures[2], Is.EqualTo((FontFeatures.StandardLigatures, true)));
     }
     
     #endregion
