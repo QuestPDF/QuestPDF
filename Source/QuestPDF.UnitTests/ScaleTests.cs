5946d08aac422458d40930f2d7f87e90c531d36e
Marcin Ziąbek <marcin@ziabek.com>
2025-09-29 15:21:30 +0200

    Add tests for Scale element

--- Source/QuestPDF.UnitTests/ScaleTests.cs
+++ Source/QuestPDF.UnitTests/ScaleTests.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using QuestPDF.Drawing;
 using QuestPDF.Elements;
+using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
 using QuestPDF.UnitTests.TestEngine;
 
@@ -9,144 +11,137 @@ namespace QuestPDF.UnitTests
     [TestFixture]
     public class ScaleTests
     {
-        #region measure
+        [TestCase(2, 2, ExpectedResult = "A=2")]
+        [TestCase(-3, -3, ExpectedResult = "A=-3")]
+        [TestCase(4, 1, ExpectedResult = "H=4")]
+        [TestCase(1, -2, ExpectedResult = "V=-2")]
+        [TestCase(4, 5, ExpectedResult = "H=4   V=5")]
+        [TestCase(1.2345f, -2.3456f, ExpectedResult = "H=1.2   V=-2.3")]
+        public string CompanionHint(float horizontal, float vertical)
+        {
+            var container = EmptyContainer.Create();
+
+            container.ScaleHorizontal(horizontal).ScaleVertical(vertical);
+            
+            var translationElement = container.Child as Scale;
+            return translationElement?.GetCompanionHint();
+        }     
+        
+        #region Cumulative Property
         
         [Test]
-        public void Measure_Wrap()
+        public void HorizontalScaleIsCumulative()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 3,
-                    ScaleY = 2
-                })
-                .MeasureElement(new Size(900, 800))
-                .ExpectChildMeasure(new Size(300, 400), SpacePlan.Wrap("Mock"))
-                .CheckMeasureResult(SpacePlan.Wrap("Forwarded from child"));
+            var container = EmptyContainer.Create();
+        
+            container.ScaleHorizontal(3).ScaleHorizontal(0.5f).ScaleHorizontal(-4);
+        
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(-6));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(1));
         }
         
         [Test]
-        public void Measure_PartialRender()
+        public void VerticalScaleIsCumulative()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 3,
-                    ScaleY = 2
-                })
-                .MeasureElement(new Size(900, 800))
-                .ExpectChildMeasure(new Size(300, 400), SpacePlan.PartialRender(200, 350))
-                .CheckMeasureResult(SpacePlan.PartialRender(600, 700));
+            var container = EmptyContainer.Create();
+        
+            container.ScaleVertical(2).ScaleVertical(-0.25f).ScaleVertical(-3f);
+        
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(1));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(1.5f));
         }
         
         [Test]
-        public void Measure_FullRender()
+        public void ScaleIsCumulative()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 3,
-                    ScaleY = 2
-                })
-                .MeasureElement(new Size(900, 800))
-                .ExpectChildMeasure(new Size(300, 400), SpacePlan.FullRender(250, 300))
-                .CheckMeasureResult(SpacePlan.FullRender(750, 600));
-        }
+            var container = EmptyContainer.Create();
         
-        [Test]
-        public void Measure_NegativeScaleX()
-        {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = -2,
-                    ScaleY = 1
-                })
-                .MeasureElement(new Size(800, 600))
-                .ExpectChildMeasure(new Size(400, 600), SpacePlan.FullRender(300, 500))
-                .CheckMeasureResult(SpacePlan.FullRender(600, 500));
-        }
+            container.ScaleHorizontal(-5f).ScaleVertical(3).Scale(-0.25f);
         
-        [Test]
-        public void Measure_NegativeScaleY()
-        {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 1,
-                    ScaleY = -3
-                })
-                .MeasureElement(new Size(800, 600))
-                .ExpectChildMeasure(new Size(800, 200), SpacePlan.FullRender(800, 100))
-                .CheckMeasureResult(SpacePlan.FullRender(800, 300));
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(1.25f));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(-0.75f));
         }
         
         #endregion
         
-        #region draw
+        #region Flip
         
         [Test]
-        public void Draw_Simple()
+        public void FlipHorizontalAppliesCorrectScale()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 3,
-                    ScaleY = 2
-                })
-                .DrawElement(new Size(900, 800))
-                .ExpectCanvasTranslate(0, 0)
-                .ExpectCanvasScale(3, 2)
-                .ExpectChildDraw(new Size(300, 400))
-                .ExpectCanvasScale(1/3f, 1/2f)
-                .ExpectCanvasTranslate(0, 0)
-                .CheckDrawResult();
+            var container = EmptyContainer.Create();
+        
+            container.FlipHorizontal();
+        
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(-1));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(1));
         }
         
         [Test]
-        public void Draw_NegativeScaleX()
+        public void FlipVerticalAppliesCorrectScale()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = -3,
-                    ScaleY = 2
-                })
-                .DrawElement(new Size(900, 800))
-                .ExpectCanvasTranslate(900, 0)
-                .ExpectCanvasScale(-3, 2)
-                .ExpectChildDraw(new Size(300, 400))
-                .ExpectCanvasScale(-1/3f, 1/2f)
-                .ExpectCanvasTranslate(-900, 0)
-                .CheckDrawResult();
+            var container = EmptyContainer.Create();
+        
+            container.FlipVertical();
+        
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(1));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(-1));
         }
         
         [Test]
-        public void Draw_NegativeScaleY()
+        public void FlipOverAppliesCorrectScale()
         {
-            TestPlan
-                .For(x => new Scale
-                {
-                    Child = x.CreateChild(),
-                    ScaleX = 3,
-                    ScaleY = -2
-                })
-                .DrawElement(new Size(900, 800))
-                .ExpectCanvasTranslate(0, 800)
-                .ExpectCanvasScale(3, -2)
-                .ExpectChildDraw(new Size(300, 400))
-                .ExpectCanvasScale(1/3f, -1/2f)
-                .ExpectCanvasTranslate(0, -800)
-                .CheckDrawResult();
+            var container = EmptyContainer.Create();
+        
+            container.FlipOver();
+        
+            var rowContainer = container.Child as Scale;
+            Assert.That(rowContainer?.ScaleX, Is.EqualTo(-1));
+            Assert.That(rowContainer?.ScaleY, Is.EqualTo(-1));
         }
         
+        #endregion
+
+        #region Zero Scale Validation
+
+        [Test]
+        public void ScaleCannotBeZero()
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                EmptyContainer.Create().Scale(0);
+            });
+            
+            Assert.That(exception.Message, Is.EqualTo("Vertical scale factor cannot be zero. (Parameter 'factor')"));
+        }
+        
+        [Test]
+        public void VerticalScaleCannotBeZero()
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                EmptyContainer.Create().ScaleVertical(0);
+            });
+            
+            Assert.That(exception.Message, Is.EqualTo("Vertical scale factor cannot be zero. (Parameter 'factor')"));
+        }
+        
+        [Test]
+        public void HorizontalScaleCannotBeZero()
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                EmptyContainer.Create().ScaleHorizontal(0);
+            });
+            
+            Assert.That(exception.Message, Is.EqualTo("Vertical scale factor cannot be zero. (Parameter 'factor')"));
+        }
+
         #endregion
     }
 }
