e5eb9366f4145f7793db01b906b580a268312229
Marcin Ziąbek <marcin@ziabek.com>
2025-09-30 14:31:02 +0200

    Load Latin words from a resource file instead of hardcoding them (removes false positives from mutation testing)

--- Source/QuestPDF/Helpers/Placeholders.cs
+++ Source/QuestPDF/Helpers/Placeholders.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using QuestPDF.Infrastructure;
 using QuestPDF.Skia;
 
@@ -25,41 +27,22 @@ namespace QuestPDF.Helpers
             "occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
             "mollit anim id est laborum.";
 
-        private static readonly string[] LatinWords =
-        {
-            "exercitationem", "perferendis", "perspiciatis", "laborum", "eveniet",
-            "sunt", "iure", "nam", "nobis", "eum", "cum", "officiis", "excepturi",
-            "odio", "consectetur", "quasi", "aut", "quisquam", "vel", "eligendi",
-            "itaque", "non", "odit", "tempore", "quaerat", "dignissimos",
-            "facilis", "neque", "nihil", "expedita", "vitae", "vero", "ipsum",
-            "nisi", "animi", "cumque", "pariatur", "velit", "modi", "natus",
-            "iusto", "eaque", "sequi", "illo", "sed", "ex", "et", "voluptatibus",
-            "tempora", "veritatis", "ratione", "assumenda", "incidunt", "nostrum",
-            "placeat", "aliquid", "fuga", "provident", "praesentium", "rem",
-            "necessitatibus", "suscipit", "adipisci", "quidem", "possimus",
-            "voluptas", "debitis", "sint", "accusantium", "unde", "sapiente",
-            "voluptate", "qui", "aspernatur", "laudantium", "soluta", "amet",
-            "quo", "aliquam", "saepe", "culpa", "libero", "ipsa", "dicta",
-            "reiciendis", "nesciunt", "doloribus", "autem", "impedit", "minima",
-            "maiores", "repudiandae", "ipsam", "obcaecati", "ullam", "enim",
-            "totam", "delectus", "ducimus", "quis", "voluptates", "dolores",
-            "molestiae", "harum", "dolorem", "quia", "voluptatem", "molestias",
-            "magni", "distinctio", "omnis", "illum", "dolorum", "voluptatum", "ea",
-            "quas", "quam", "corporis", "quae", "blanditiis", "atque", "deserunt",
-            "laboriosam", "earum", "consequuntur", "hic", "cupiditate",
-            "quibusdam", "accusamus", "ut", "rerum", "error", "minus", "eius",
-            "ab", "ad", "nemo", "fugit", "officia", "at", "in", "id", "quos",
-            "reprehenderit", "numquam", "iste", "fugiat", "sit", "inventore",
-            "beatae", "repellendus", "magnam", "recusandae", "quod", "explicabo",
-            "doloremque", "aperiam", "consequatur", "asperiores", "commodi",
-            "optio", "dolor", "labore", "temporibus", "repellat", "veniam",
-            "architecto", "est", "esse", "mollitia", "nulla", "a", "similique",
-            "eos", "alias", "dolore", "tenetur", "deleniti", "porro", "facere",
-            "maxime", "corrupti"
-        };
+        private static readonly string[] LatinWords = LoadLatinWords();
         
         private static readonly string[] LongLatinWords = LatinWords.Where(x => x.Length > 8).ToArray();
 
+        private static string[] LoadLatinWords()
+        {
+            using var stream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("QuestPDF.Resources.LatinWords.txt");
+        
+            using var streamReader = new StreamReader(stream);
+            var text = streamReader.ReadToEnd();
+
+            return text.Split('\n').ToArray();
+        }
+        
         #endregion
 
         #region Text
