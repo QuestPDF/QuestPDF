d1b8992c8d7db05d48f835aa4fcbd83fd9e7284e
Marcin Ziąbek <marcin@ziabek.com>
2025-10-14 13:54:19 +0200

    Refactor: streamline document container debugging and semantic part handling

--- Source/QuestPDF/Drawing/DocumentContainer.cs
+++ Source/QuestPDF/Drawing/DocumentContainer.cs
@@ -22,9 +22,7 @@ namespace QuestPDF.Drawing
                 if (Pages.Count == 0)
                     Pages.Add(new Page());
 
-                container = container
-                    .DebugPointer(DebugPointerType.DocumentStructure, DocumentStructureTypes.Document.ToString())
-                    .SemanticDocument();
+                container = container.DebugPointer(DebugPointerType.DocumentStructure, DocumentStructureTypes.Document.ToString());
                 
                 if (Pages.Count == 1)
                 {
@@ -39,7 +37,7 @@ namespace QuestPDF.Drawing
                             .SelectMany(x => new List<Action>()
                             {
                                 () => column.Item().PageBreak(),
-                                () => column.Item().Component(x)
+                                () => column.Item().SemanticPart().Component(x)
                             })
                             .Skip(1)
                             .ToList()
