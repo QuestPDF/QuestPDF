8bc45e948fa7dd4c5f389e420f6a5f22af5d93b9
Marcin Ziąbek <marcin@ziabek.com>
2025-10-28 11:30:26 +0100

    Missing files

---
+++ Source/QuestPDF.InteropGenerators.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace QuestPDF.InteropGenerators.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        await Program.Main();
+    }
+}
