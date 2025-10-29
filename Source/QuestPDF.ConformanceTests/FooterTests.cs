a089378b4b6b0f6bc0b3c3f37166d88a0bce1a74
Marcin Ziąbek <marcin@ziabek.com>
2025-10-29 11:41:40 +0100

    Add Semantic Test Scaffolding

---
+++ Source/QuestPDF.ConformanceTests/FooterTests.cs
@@ -0,0 +1,18 @@
+using QuestPDF.ConformanceTests.TestEngine;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+
+namespace QuestPDF.ConformanceTests;
+
+internal class FooterTests : ConformanceTestBase
+{
+    protected override Document GetDocumentUnderTest()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override SemanticTreeNode? GetExpectedSemanticTree()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Source/QuestPDF.ConformanceTests/StyledBoxTests.cs
@@ -0,0 +1,18 @@
+using QuestPDF.ConformanceTests.TestEngine;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+
+namespace QuestPDF.ConformanceTests;
+
+internal class StyledBoxTests : ConformanceTestBase
+{
+    protected override Document GetDocumentUnderTest()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override SemanticTreeNode? GetExpectedSemanticTree()
+    {
+        throw new NotImplementedException();
+    }
+}
