7215136a5bb4fe4a1087501058a53baad4ecd7ae
Marcin Ziąbek <marcin@ziabek.com>
2025-10-30 10:32:26 +0100

    Implement conformance tests for the Footer element

--- Source/QuestPDF.ConformanceTests/FooterTests.cs
+++ Source/QuestPDF.ConformanceTests/FooterTests.cs
@@ -1,6 +1,7 @@
 using QuestPDF.ConformanceTests.TestEngine;
 using QuestPDF.Drawing;
 using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 
 namespace QuestPDF.ConformanceTests;
 
@@ -8,11 +9,71 @@ internal class FooterTests : ConformanceTestBase
 {
     protected override Document GetDocumentUnderTest()
     {
-        throw new NotImplementedException();
+        return Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.Margin(60);
+
+                    page.Content()
+                        .PaddingBottom(25)
+                        .Column(column =>
+                        {
+                            column.Spacing(25);
+
+                            column.Item()
+                                .SemanticHeader1()
+                                .Text("Conformance Test: Footer")
+                                .FontSize(24)
+                                .Bold()
+                                .FontColor(Colors.Blue.Darken2);
+
+                            column.Item()
+                                .Text("Footer content should not be present in the semantic tree.");
+
+                            column.Item()
+                                .SemanticDivision()
+                                .Column(column =>
+                                {
+                                    foreach (var i in Enumerable.Range(1, 12))
+                                    {
+                                        column.Item()
+                                            .Width(200)
+                                            .Height(100)
+                                            .Background(Colors.Grey.Lighten2)
+                                            .AlignCenter()
+                                            .AlignMiddle()
+                                            .Text($"Item {i}");
+                                    }
+                                });
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(text =>
+                        {
+                            text.CurrentPageNumber();
+                            text.Span(" / ");
+                            text.TotalPages();
+                        });
+                });
+            });
     }
 
     protected override SemanticTreeNode? GetExpectedSemanticTree()
     {
-        throw new NotImplementedException();
+        return ExpectedSemanticTree.DocumentRoot(root =>
+        {
+            root.Child("H1", h1 => h1.Alt("Conformance Test: Footer"));
+            
+            root.Child("P");
+
+            root.Child("Div", div =>
+            {
+                foreach (var i in Enumerable.Range(1, 12))
+                    div.Child("P");
+            });
+        });
     }
 }
