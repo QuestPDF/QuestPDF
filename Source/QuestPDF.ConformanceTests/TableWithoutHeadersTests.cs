376dc3497ae195cd88b7dd212d006c9b415136d6
Marcin Ziąbek <marcin@ziabek.com>
2025-10-30 09:10:21 +0100

    Add tests for tables without headers in Conformance Tests

---
+++ Source/QuestPDF.ConformanceTests/TableWithoutHeadersTests.cs
@@ -0,0 +1,103 @@
+using QuestPDF.ConformanceTests.TestEngine;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.ConformanceTests;
+
+internal class TableWithoutHeadersTests : ConformanceTestBase
+{
+    protected override Document GetDocumentUnderTest()
+    {
+        return Document
+            .Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.Margin(60);
+
+                    page.Content()
+                        .Border(1)
+                        .BorderColor(Colors.Grey.Darken1)
+                        .Table(table =>
+                        {
+                            table.ApplySemanticTags();
+
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.RelativeColumn();
+                                columns.RelativeColumn();
+                                columns.RelativeColumn();
+                            });
+
+                            // Row 1
+                            table.Cell().Element(CellStyle).Text("11");
+                            table.Cell().Element(CellStyle).Text("12");
+                            table.Cell().Element(CellStyle).Text("13");
+
+                            // Row 2
+                            table.Cell().Element(CellStyle).Text("21");
+                            table.Cell().Element(CellStyle).Text("22");
+                            table.Cell().Element(CellStyle).Text("23");
+
+                            // Row 3
+                            table.Cell().Element(CellStyle).Text("31");
+                            table.Cell().Element(CellStyle).Text("32");
+                            table.Cell().Element(CellStyle).Text("33");
+
+                            // Row 4
+                            table.Cell().Element(CellStyle).Text("41");
+                            table.Cell().Element(CellStyle).Text("42");
+                            table.Cell().Element(CellStyle).Text("43");
+                            
+                            IContainer CellStyle(IContainer container) =>
+                                container
+                                    .Border(1)
+                                    .BorderColor(Colors.Grey.Lighten2)
+                                    .Padding(8);
+                        });
+                });
+            });
+    }
+
+    protected override SemanticTreeNode? GetExpectedSemanticTree()
+    {
+        return ExpectedSemanticTree.DocumentRoot(root =>
+        {
+            root.Child("Table", table =>
+            {
+                table.Child("TBody", tbody =>
+                {
+                    tbody.Child("TR", row =>
+                    {
+                        row.Child("TD", th => th.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                    });
+
+                    tbody.Child("TR", row =>
+                    {
+                        row.Child("TD", th => th.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                    });
+
+                    tbody.Child("TR", row =>
+                    {
+                        row.Child("TD", th => th.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                    });
+
+                    tbody.Child("TR", row =>
+                    {
+                        row.Child("TD", th => th.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                        row.Child("TD", td => td.Child("P"));
+                    });
+                });
+            });
+        });
+    }
+}
