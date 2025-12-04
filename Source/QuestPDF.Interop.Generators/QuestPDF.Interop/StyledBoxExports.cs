e9bf3383368b9544138b796b5fef4299749b5272
Marcin Ziąbek <marcin@ziabek.com>
2025-12-04 09:32:56 +0100

    Interop: add support for background and border gradients

---
+++ Source/QuestPDF.Interop.Generators/QuestPDF.Interop/StyledBoxExports.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.Interop;
+
+public unsafe partial class Exports
+{
+    [UnmanagedCallersOnly(EntryPoint = "questpdf__container__background_linear_gradient", CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static IntPtr Container_BackgroundLinearGradient(IntPtr target, float angle, uint* colors, int colorsLength)
+    {
+        var targetObject = UnboxHandle<IContainer>(target);
+
+        var colorArray = new Color[colorsLength];
+        
+        for (var i = 0; i < colorsLength; i++)
+            colorArray[i] = colors[i];
+    
+        var result = targetObject.BackgroundLinearGradient(angle, colorArray);
+        return BoxHandle(result);
+    }
+    
+    [UnmanagedCallersOnly(EntryPoint = "questpdf__container__border_linear_gradient", CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static IntPtr Container_BorderLinearGradient(IntPtr target, float angle, uint* colors, int colorsLength)
+    {
+        var targetObject = UnboxHandle<IContainer>(target);
+    
+        var colorArray = new Color[colorsLength];
+        
+        for (var i = 0; i < colorsLength; i++)
+            colorArray[i] = colors[i];
+        
+        var result = targetObject.BorderLinearGradient(angle, colorArray);
+        return BoxHandle(result);
+    }
+}
