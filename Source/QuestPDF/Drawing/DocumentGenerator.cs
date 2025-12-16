a9daed4b7f4ff6fade22534bb46a75e2ab06a1f1
kubi04 <122866531+BS-yzl-KubilaydelRioFernandez@users.noreply.github.com>
2025-12-16 07:42:08 +0100

    Handle outdated semantic tags for PDF/A-1A

--- Source/QuestPDF/Drawing/DocumentGenerator.cs
+++ Source/QuestPDF/Drawing/DocumentGenerator.cs
@@ -22,58 +22,58 @@ namespace QuestPDF.Drawing
         {
             SkNativeDependencyCompatibilityChecker.Test();
         }
-        
+
         internal static void GeneratePdf(SkWriteStream stream, IDocument document)
         {
             ValidateLicense();
-            
+
             var metadata = document.GetMetadata();
             var settings = document.GetSettings();
 
             using var canvas = new PdfDocumentCanvas(stream, metadata, settings);
             RenderDocument(canvas, document, settings);
         }
-        
+
         internal static void GenerateXps(SkWriteStream stream, IDocument document)
         {
             ValidateLicense();
-            
+
             var settings = document.GetSettings();
             using var canvas = new XpsDocumentCanvas(stream, settings);
             RenderDocument(canvas, document, settings);
         }
-        
+
         internal static ICollection<byte[]> GenerateImages(IDocument document, ImageGenerationSettings imageGenerationSettings)
         {
             ValidateLicense();
-            
+
             var documentSettings = document.GetSettings();
             documentSettings.ImageRasterDpi = imageGenerationSettings.RasterDpi;
-            
+
             using var canvas = new ImageDocumentCanvas(imageGenerationSettings);
             RenderDocument(canvas, document, documentSettings);
 
             return canvas.Images;
         }
-        
+
         internal static ICollection<string> GenerateSvg(IDocument document)
         {
             ValidateLicense();
-            
+
             using var canvas = new SvgDocumentCanvas();
             RenderDocument(canvas, document, document.GetSettings());
 
             return canvas.Images;
         }
-        
+
         internal static void ValidateLicense()
         {
             if (Settings.License.HasValue)
                 return;
-            
+
             const string newParagraph = "\n\n";
 
-            var exceptionMessage = 
+            var exceptionMessage =
                 $"{newParagraph}{newParagraph}Welcome to QuestPDF! 👋 {newParagraph}" +
                 $"QuestPDF is an open-source library committed to long-term sustainability and continuous improvement. {newParagraph}" +
                 $"To maintain high-quality development and support while keeping the library free for most users, we use a fair pricing model where only larger organizations help by providing necessary funding for the project. {newParagraph}" +
@@ -83,7 +83,7 @@ namespace QuestPDF.Drawing
                 $"We trust our users. To continue, no license key is needed. Instead, simply select and configure the appropriate license in your code. For example, if you qualify for the Community license, add: {newParagraph}" +
                 $"> QuestPDF.Settings.License = LicenseType.Community; {newParagraph}" +
                 $"Thank you for supporting QuestPDF! ❤️ By choosing the right license, you help ensure that our project remains transparent, sustainable, and continuously improving for everyone. 🚀 {newParagraph}{newParagraph}";
-            
+
             throw new Exception(exceptionMessage)
             {
                 HelpLink = "https://www.questpdf.com/pricing.html"
@@ -104,26 +104,26 @@ namespace QuestPDF.Drawing
                 RenderMergedDocument(canvas, mergedDocument, settings);
                 return;
             }
-            
+
             var semanticTreeManager = CreateSemanticTreeManager(settings);
             var useOriginalImages = canvas is ImageDocumentCanvas;
             var content = ConfigureContent(document, settings, semanticTreeManager, useOriginalImages);
-            
+
             if (canvas is CompanionDocumentCanvas)
                 content.VisitChildren(x => x.CreateProxy(y => new LayoutProxy(y)));
-            
+
             try
             {
                 var pageContext = new PageContext();
                 RenderPass(pageContext, new FreeDocumentCanvas(), content);
                 pageContext.ProceedToNextRenderingPhase();
 
-                canvas.ConfigureWithSemanticTree(semanticTreeManager);
-                
+                canvas.ConfigureWithSemanticTree(semanticTreeManager, settings);
+
                 canvas.BeginDocument();
                 RenderPass(pageContext, canvas, content);
                 canvas.EndDocument();
-            
+
                 if (canvas is CompanionDocumentCanvas companionCanvas)
                     companionCanvas.Hierarchy = content.ExtractHierarchy();
             }
@@ -132,16 +132,16 @@ namespace QuestPDF.Drawing
                 content.ReleaseDisposableChildren();
             }
         }
-        
+
         private static void RenderMergedDocument(IDocumentCanvas canvas, MergedDocument document, DocumentSettings settings)
         {
             var useOriginalImages = canvas is ImageDocumentCanvas;
-            
+
             var sharedPageContent = new PageContext();
             var useSharedPageContext = document.PageNumberStrategy == MergedDocumentPageNumberStrategy.Continuous;
 
             var semanticTreeManager = CreateSemanticTreeManager(settings);
-            
+
             var documentParts = Enumerable
                 .Range(0, document.Documents.Count)
                 .Select(index => new
@@ -151,20 +151,20 @@ namespace QuestPDF.Drawing
                     PageContext = useSharedPageContext ? sharedPageContent : new PageContext()
                 })
                 .ToList();
-            
+
             try
             {
                 foreach (var documentPart in documentParts)
                     documentPart.PageContext.SetDocumentId(documentPart.DocumentId);
-                
+
                 foreach (var documentPart in documentParts)
                 {
                     RenderPass(documentPart.PageContext, new FreeDocumentCanvas(), documentPart.Content);
                     documentPart.PageContext.ProceedToNextRenderingPhase();
                 }
 
-                canvas.ConfigureWithSemanticTree(semanticTreeManager);
-                
+                canvas.ConfigureWithSemanticTree(semanticTreeManager, settings);
+
                 canvas.BeginDocument();
 
                 foreach (var documentPart in documentParts)
@@ -172,7 +172,7 @@ namespace QuestPDF.Drawing
                     RenderPass(documentPart.PageContext, canvas, documentPart.Content);
                     documentPart.Content.ReleaseDisposableChildren();
                 }
-                
+
                 canvas.EndDocument();
             }
             finally
@@ -190,19 +190,23 @@ namespace QuestPDF.Drawing
                 if (settings.PDFUA_Conformance is not PDFUA_Conformance.None)
                     return true;
 
-                //if (settings.PDFA_Conformance is PDFA_Conformance.PDFA_1A or PDFA_Conformance.PDFA_2A or PDFA_Conformance.PDFA_3A)
-                if (settings.PDFA_Conformance is PDFA_Conformance.PDFA_2A or PDFA_Conformance.PDFA_3A)
+                if (settings.PDFA_Conformance is PDFA_Conformance.PDFA_1A or PDFA_Conformance.PDFA_2A or PDFA_Conformance.PDFA_3A)
                     return true;
 
                 return false;
             }
         }
 
-        private static void ConfigureWithSemanticTree(this IDocumentCanvas canvas, SemanticTreeManager? semanticTreeManager)
+        private static void ConfigureWithSemanticTree(this IDocumentCanvas canvas, SemanticTreeManager? semanticTreeManager, DocumentSettings settings)
         {
-            if (semanticTreeManager == null) 
+            if (semanticTreeManager == null)
                 return;
-            
+
+            if (settings.PDFA_Conformance is PDFA_Conformance.PDFA_1A)
+            {
+                semanticTreeManager.RemoveNonStandardStructureTypes();
+            }
+
             var semanticTree = semanticTreeManager.GetSemanticTree();
             semanticTreeManager.Reset();
             canvas.SetSemanticTree(semanticTree);
@@ -212,22 +216,22 @@ namespace QuestPDF.Drawing
         {
             var container = new DocumentContainer();
             document.Compose(container);
-            
+
             var content = container.Compose();
-            
+
             content.ApplyInheritedAndGlobalTexStyle(TextStyle.Default);
             content.ApplyContentDirection(settings.ContentDirection);
             content.ApplyDefaultImageConfiguration(settings.ImageRasterDpi, settings.ImageCompressionQuality, useOriginalImages);
 
             if (Settings.EnableCaching)
                 content.ApplyCaching();
-            
+
             if (semanticTreeManager != null)
             {
                 content.ApplySemanticParagraphs();
                 content.InjectSemanticTreeManager(semanticTreeManager);
             }
-            
+
             return content;
         }
 
@@ -236,7 +240,7 @@ namespace QuestPDF.Drawing
             content.InjectDependencies(pageContext, canvas.GetDrawingCanvas());
             content.VisitChildren(x => (x as IStateful)?.ResetState(hardReset: true));
 
-            while(true)
+            while (true)
             {
                 pageContext.IncrementPageNumber();
                 var spacePlan = content.Measure(Size.Max);
@@ -246,13 +250,13 @@ namespace QuestPDF.Drawing
                     pageContext.DecrementPageNumber();
                     canvas.EndDocument();
 
-                    #if NET6_0_OR_GREATER
+#if NET6_0_OR_GREATER
                     if (!CompanionService.IsCompanionAttached)
                         ThrowLayoutException();
-                    #else
+#else
                     ThrowLayoutException();
-                    #endif
-                    
+#endif
+
                     ApplyLayoutDebugging();
                 }
 
@@ -272,7 +276,7 @@ namespace QuestPDF.Drawing
                 if (spacePlan.Type == SpacePlanType.FullRender)
                     break;
             }
-            
+
             void ApplyLayoutDebugging()
             {
                 content.VisitChildren(x => (x as SnapshotCacheRecorderProxy)?.Dispose());
@@ -284,25 +288,25 @@ namespace QuestPDF.Drawing
                 var overflowState = content.ExtractElementsOfType<OverflowDebuggingProxy>().Single();
                 overflowState.StopMeasuring();
                 overflowState.TryToFixTheLayoutOverflowIssue();
-                
+
                 content.ApplyContentDirection();
                 content.InjectDependencies(pageContext, canvas.GetDrawingCanvas());
 
                 content.VisitChildren(x => (x as LayoutProxy)?.CaptureLayoutErrorMeasurement());
                 content.RemoveExistingProxiesOfType<OverflowDebuggingProxy>();
             }
-            
+
             void ThrowLayoutException()
             {
                 var newLine = "\n";
                 var newParagraph = newLine + newLine;
-                
+
                 const string debuggingSettingsName = $"{nameof(QuestPDF)}.{nameof(Settings)}.{nameof(Settings.EnableDebugging)}";
 
                 var message =
                     $"The provided document content contains conflicting size constraints. " +
                     $"For example, some elements may require more space than is available. {newParagraph}";
-                
+
                 if (Settings.EnableDebugging)
                 {
                     var (ancestors, layout) = GenerateLayoutExceptionDebuggingInfo();
@@ -322,22 +326,22 @@ namespace QuestPDF.Drawing
                         $"To further investigate the location of the root cause, please run the application with a debugger attached or set the {debuggingSettingsName} flag to true. " +
                         $"The library will generate additional debugging information such as probable code problem location and detailed layout measurement overview.";
                 }
-                
+
                 throw new DocumentLayoutException(message);
             }
-            
+
             (ICollection<Element> ancestors, TreeNode<OverflowDebuggingProxy> layout) GenerateLayoutExceptionDebuggingInfo()
             {
                 content.RemoveExistingProxies();
                 content.ApplyLayoutOverflowDetection();
                 content.Measure(Size.Max);
-                
+
                 var overflowState = content.ExtractElementsOfType<OverflowDebuggingProxy>().Single();
                 overflowState.StopMeasuring();
                 overflowState.TryToFixTheLayoutOverflowIssue();
 
                 var rootCause = overflowState.FindLayoutOverflowVisualizationNodes().First();
-                
+
                 var ancestors = rootCause
                     .ExtractAncestors()
                     .Select(x => x.Value.Child)
@@ -372,23 +376,23 @@ namespace QuestPDF.Drawing
                 }
             });
         }
-        
+
         internal static void InjectDependencies(this Element content, IPageContext pageContext, IDrawingCanvas canvas)
         {
             content.VisitChildren(x =>
             {
                 if (x == null)
                     return;
-                
+
                 x.PageContext = pageContext;
                 x.Canvas = canvas;
             });
         }
-        
+
         internal static void ApplyCaching(this Element? content)
         {
             var canApplyCaching = Traverse(content);
-            
+
             if (canApplyCaching)
                 content?.CreateProxy(x => new SnapshotCacheRecorderProxy(x));
 
@@ -401,7 +405,7 @@ namespace QuestPDF.Drawing
                     {
                         if (textBlockItem is TextBlockPageNumber)
                             return false;
-                        
+
                         if (textBlockItem is TextBlockElement textBlockElement && !Traverse(textBlockElement.Element))
                             return false;
                     }
@@ -414,22 +418,22 @@ namespace QuestPDF.Drawing
 
                 if (content is DynamicHost)
                     return false;
-                
+
                 if (content is ContainerElement containerElement)
                     return Traverse(containerElement.Child);
 
                 if (content is MultiColumn multiColumn)
                 {
                     var multiColumnSupportsCaching = Traverse(multiColumn.Content) && Traverse(multiColumn.Spacer);
-                    
+
                     multiColumn.Content.RemoveExistingProxies();
                     multiColumn.Spacer.RemoveExistingProxies();
-                    
+
                     return multiColumnSupportsCaching;
                 }
 
                 var canApplyCachingPerChild = content.GetChildren().Select(Traverse).ToArray();
-                
+
                 if (canApplyCachingPerChild.All(x => x))
                     return true;
 
@@ -437,7 +441,7 @@ namespace QuestPDF.Drawing
                     return false;
 
                 var childIndex = 0;
-                
+
                 content.CreateProxy(x =>
                 {
                     var canApplyCaching = canApplyCachingPerChild[childIndex];
@@ -445,11 +449,11 @@ namespace QuestPDF.Drawing
 
                     return canApplyCaching ? new SnapshotCacheRecorderProxy(x) : x;
                 });
-                
+
                 return false;
             }
         }
-        
+
         internal static void ApplyContentDirection(this Element? content, ContentDirection? direction = null)
         {
             if (content == null)
@@ -463,11 +467,11 @@ namespace QuestPDF.Drawing
 
             if (content is IContentDirectionAware contentDirectionAware)
                 contentDirectionAware.ContentDirection = direction ?? contentDirectionAware.ContentDirection;
-            
+
             foreach (var child in content.GetChildren())
                 ApplyContentDirection(child, direction);
         }
-        
+
         internal static void ApplyDefaultImageConfiguration(this Element? content, int imageRasterDpi, ImageCompressionQuality imageCompressionQuality, bool useOriginalImages)
         {
             content.VisitChildren(x =>
@@ -492,7 +496,7 @@ namespace QuestPDF.Drawing
                     dynamicHost.ImageCompressionQuality ??= imageCompressionQuality;
                     dynamicHost.UseOriginalImage |= useOriginalImages;
                 }
-                
+
                 if (x is Lazy lazy)
                 {
                     lazy.ImageTargetDpi ??= imageRasterDpi;
@@ -514,31 +518,31 @@ namespace QuestPDF.Drawing
         {
             if (content == null)
                 return;
-            
+
             if (content is TextBlock textBlock)
             {
                 textBlock.DefaultTextStyle = textBlock.DefaultTextStyle.ApplyInheritedStyle(documentDefaultTextStyle).ApplyGlobalStyle();
-                
+
                 foreach (var textBlockItem in textBlock.Items)
                 {
                     if (textBlockItem is TextBlockSpan textSpan)
                         textSpan.Style = textSpan.Style.ApplyInheritedStyle(documentDefaultTextStyle).ApplyGlobalStyle();
-                    
+
                     if (textBlockItem is TextBlockElement textElement)
                         ApplyInheritedAndGlobalTexStyle(textElement.Element, documentDefaultTextStyle);
                 }
-                
+
                 return;
             }
 
             if (content is DynamicHost dynamicHost)
                 dynamicHost.TextStyle = dynamicHost.TextStyle.ApplyInheritedStyle(documentDefaultTextStyle);
-            
+
             if (content is Lazy lazy)
                 lazy.TextStyle = lazy.TextStyle.ApplyInheritedStyle(documentDefaultTextStyle);
-            
+
             if (content is DefaultTextStyle defaultTextStyleElement)
-               documentDefaultTextStyle = defaultTextStyleElement.TextStyle.ApplyInheritedStyle(documentDefaultTextStyle);
+                documentDefaultTextStyle = defaultTextStyleElement.TextStyle.ApplyInheritedStyle(documentDefaultTextStyle);
 
             if (content is ContainerElement containerElement)
             {
@@ -554,9 +558,9 @@ namespace QuestPDF.Drawing
         internal static void ApplySemanticParagraphs(this Element root)
         {
             var isFooterContext = false;
-            
+
             Traverse(root);
-            
+
             void Traverse(Element element)
             {
                 if (element is SemanticTag { TagType: "H" or "H1" or "H2" or "H3" or "H4" or "H5" or "H6" or "P" or "Lbl" })
@@ -578,10 +582,10 @@ namespace QuestPDF.Drawing
                     if (container.Child is TextBlock textBlock)
                     {
                         var textBlockContainsPageNumber = textBlock.Items.Any(x => x is TextBlockPageNumber);
-                        
+
                         if (isFooterContext && textBlockContainsPageNumber)
                             return;
-                        
+
                         container.CreateProxy(x => new SemanticTag
                         {
                             Child = x,
