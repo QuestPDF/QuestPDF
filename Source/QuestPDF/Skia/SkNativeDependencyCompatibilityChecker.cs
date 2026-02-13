9e9ca00c07c15c2e537a6855c24a677fb6651f03
Marcin Ziąbek <marcin@ziabek.com>
2026-02-13 14:28:08 +0100

    Update skia native dependency to m144

--- Source/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
+++ Source/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
@@ -7,7 +7,7 @@ namespace QuestPDF.Skia;
 
 internal static class SkNativeDependencyCompatibilityChecker
 {
-    private const int ExpectedNativeLibraryVersion = 9;
+    private const int ExpectedNativeLibraryVersion = 11;
     
     private static NativeDependencyCompatibilityChecker Instance { get; } = new()
     {
