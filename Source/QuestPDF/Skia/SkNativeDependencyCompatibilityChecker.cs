f1e8a7b1f5933d4e295ed58756d939cbe621cdf6
Marcin Ziąbek <marcin@ziabek.com>
2026-03-05 07:09:57 +0100

    Fix visual regression when rendering complex borders

--- Source/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
+++ Source/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
@@ -7,7 +7,7 @@ namespace QuestPDF.Skia;
 
 internal static class SkNativeDependencyCompatibilityChecker
 {
-    private const int ExpectedNativeLibraryVersion = 12;
+    private const int ExpectedNativeLibraryVersion = 13;
     
     private static NativeDependencyCompatibilityChecker Instance { get; } = new()
     {
