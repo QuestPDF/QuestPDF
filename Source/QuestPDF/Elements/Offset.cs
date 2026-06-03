e457c5a0a9ea06dc55f02868cad22af043095363
MarcinZiabek <marcin@ziabek.com>
2026-06-03 22:13:47 +0200

    Rename TranslateX/TranslateY to OffsetX/OffsetY API for improved discoverability

--- Source/QuestPDF/Elements/Offset.cs
+++ Source/QuestPDF/Elements/Offset.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 namespace QuestPDF.Elements
 {
-    internal sealed class Translate : ContainerElement
+    internal sealed class Offset : ContainerElement
     {
-        public float TranslateX { get; set; } = 0;
-        public float TranslateY { get; set; } = 0;
+        public float OffsetX { get; set; } = 0;
+        public float OffsetY { get; set; } = 0;
 
         internal override void Draw(Size availableSpace)
         {
-            var translate = new Position(TranslateX, TranslateY);
-            
-            Canvas.Translate(translate);
+            var offset = new Position(OffsetX, OffsetY);
+
+            Canvas.Translate(offset);
             base.Draw(availableSpace);
-            Canvas.Translate(translate.Reverse());
+            Canvas.Translate(offset.Reverse());
         }
 
         internal override string? GetCompanionHint()
         {
             return string.Join("   ", GetOptions().Where(x => x.value != 0).Select(x => $"{x.Label}={x.value.FormatAsCompanionNumber()}"));
-            
+
             IEnumerable<(string Label, float value)> GetOptions()
             {
-                yield return ("X", TranslateX);
-                yield return ("Y", TranslateY);
+                yield return ("X", OffsetX);
+                yield return ("Y", OffsetY);
             }
         }
     }
---
+++ Source/QuestPDF/Fluent/OffsetExtensions.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using QuestPDF.Elements;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.Fluent
+{
+    public static class OffsetExtensions
+    {
+        private static IContainer Offset(this IContainer element, float x = 0, float y = 0)
+        {
+            var offset = element as Offset ?? new Offset();
+
+            offset.OffsetX += x;
+            offset.OffsetY += y;
+
+            return element.Element(offset);
+        }
+
+        /// <summary>
+        /// Moves content along the horizontal axis.
+        /// A positive value moves content to the right; a negative value moves it to the left.
+        /// Does not alter the available space.
+        /// <br />
+        /// <a href="https://www.questpdf.com/api-reference/offset.html">Learn more</a>
+        /// </summary>
+        public static IContainer OffsetX(this IContainer element, float value, Unit unit = Unit.Point)
+        {
+            value = value.ToPoints(unit);
+            return element.Offset(x: value);
+        }
+   
+        /// <summary>
+        /// Moves content along the vertical axis.
+        /// A positive value moves content downwards, a negative value moves it upwards.
+        /// Does not alter the available space.
+        /// <br />
+        /// <a href="https://www.questpdf.com/api-reference/offset.html">Learn more</a>
+        /// </summary>
+        public static IContainer OffsetY(this IContainer element, float value, Unit unit = Unit.Point)
+        {
+            value = value.ToPoints(unit);
+            return element.Offset(y: value);
+        }
+
+        #region Obsolete
+
+        [Obsolete("This element has been renamed since version 2026.6. Please use the OffsetX method.")]
+        [ExcludeFromCodeCoverage]
+        public static IContainer TranslateX(this IContainer element, float value, Unit unit = Unit.Point)
+        {
+            return element.OffsetX(value, unit);
+        }
+
+        [Obsolete("This element has been renamed since version 2026.6. Please use the OffsetY method.")]
+        [ExcludeFromCodeCoverage]
+        public static IContainer TranslateY(this IContainer element, float value, Unit unit = Unit.Point)
+        {
+            return element.OffsetY(value, unit);
+        }
+
+        #endregion
+    }
+}
