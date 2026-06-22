0ff429dbcd86402059f7fa4c4620bef6a47d2e0a
MarcinZiabek <marcin@ziabek.com>
2026-06-22 16:47:14 +0200

    Simplify Linux runtime detection by replacing `ldd`-based logic with `gnu_get_libc_version` for improved accuracy and maintenance.

--- Source/QuestPDF/Helpers/NativeRuntimeDetection.cs
+++ Source/QuestPDF/Helpers/NativeRuntimeDetection.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 
 namespace QuestPDF.Helpers;
 
@@ -35,7 +33,7 @@ internal static class NativeRuntimeDetection
                 return "win";
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return IsLinuxMusl.Value ? "linux-musl" : "linux";
+                return IsGlibcLinux.Value ? "linux" : "linux-musl";
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 return "osx";
@@ -49,34 +47,14 @@ internal static class NativeRuntimeDetection
         }
     });
 
-    private static readonly Lazy<string?> LddCommandOutput = new(() =>
+    private static readonly Lazy<string?> GlibcVersionText = new(() =>
     {
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             return null;
 
         try
         {
-            var processStartInfo = new ProcessStartInfo
-            {
-                FileName = "ldd",
-                Arguments = "--version",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using var process = Process.Start(processStartInfo);
-
-            if (process == null)
-                return null;
-
-            var standardOutputText = process.StandardOutput.ReadToEnd();
-            var standardErrorText = process.StandardError.ReadToEnd();
-
-            process.WaitForExit();
-
-            return standardOutputText + standardErrorText;
+            return Marshal.PtrToStringAnsi(gnu_get_libc_version());
         }
         catch
         {
@@ -84,47 +62,11 @@ internal static class NativeRuntimeDetection
         }
     });
 
-    private static readonly Lazy<bool> IsLinuxMusl = new(() =>
-    {
-        var lddCommandOutput = LddCommandOutput.Value ?? string.Empty;
-        return lddCommandOutput.IndexOf("musl", StringComparison.InvariantCultureIgnoreCase) >= 0;
-    });
+    private static readonly Lazy<bool> IsGlibcLinux = new(() =>
+        RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && GlibcVersionText.Value != null);
 
     public static readonly Lazy<Version?> GlibcVersion = new(() =>
-    {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            return null;
-
-        // strategy 1: use gnu_get_libc_version
-        try
-        {
-            var versionPtr = gnu_get_libc_version();
-            var versionText = Marshal.PtrToStringAnsi(versionPtr);
-            return Version.Parse(versionText);
-        }
-        catch
-        {
-            // ignore
-        }
-
-        // strategy 2: use ldd command
-        var lddCommandOutput = LddCommandOutput.Value ?? string.Empty;
-
-        if (string.IsNullOrEmpty(lddCommandOutput))
-            return null;
-
-        var match = Regex.Match(lddCommandOutput, @"ldd \(.+\) (?<version>\d+\.\d+)");
-
-        if (!match.Success)
-            return null;
-
-        var versionGroup = match.Groups["version"];
-
-        if (!versionGroup.Success)
-            return null;
-
-        return Version.TryParse(versionGroup.Value, out var parsedVersion) ? parsedVersion : null;
-    });
+        Version.TryParse(GlibcVersionText.Value, out var version) ? version : null);
 
     [DllImport("libc", EntryPoint = "gnu_get_libc_version")]
     private static extern IntPtr gnu_get_libc_version();
