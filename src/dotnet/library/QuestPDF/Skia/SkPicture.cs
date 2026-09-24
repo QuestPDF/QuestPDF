a35401860842e2c4c20e12e48c3b2b0a40edaf0d
MarcinZiabek <marcin@ziabek.com>
2026-09-24 13:32:06 +0200

    refactor(SkPicture): remove unused serialization and deserialization methods

--- src/dotnet/library/QuestPDF/Skia/SkPicture.cs
+++ src/dotnet/library/QuestPDF/Skia/SkPicture.cs
@@ -12,19 +12,7 @@ internal sealed class SkPicture : IDisposable
         Instance = instance;
         SkiaAPI.EnsureNotNull(Instance);
     }
-    
-    public static SkPicture Deserialize(SkData data)
-    {
-        var instance = API.questpdf_skia_picture_deserialize(data.Instance);
-        return new SkPicture(instance);
-    }
-    
-    public SkData Serialize()
-    {
-        var dataInstance = API.questpdf_skia_picture_serialize(Instance);
-        return new SkData(dataInstance);
-    }
-    
+
     ~SkPicture()
     {
         this.WarnThatFinalizerIsReached();
@@ -45,11 +33,5 @@ internal sealed class SkPicture : IDisposable
     {
         [DllImport(SkiaAPI.LibraryName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void questpdf_skia_picture_unref(IntPtr picture);
-        
-        [DllImport(SkiaAPI.LibraryName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr questpdf_skia_picture_serialize(IntPtr picture);
-        
-        [DllImport(SkiaAPI.LibraryName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr questpdf_skia_picture_deserialize(IntPtr data);
     }
 }
