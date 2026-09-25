3951763af4601a1ba3ecf7ad85da91f289108e8c
MarcinZiabek <marcin@ziabek.com>
2026-09-25 16:44:59 +0200

    Update expected native library version to 26 in SkNativeDependencyCompatibilityChecker

--- src/dotnet/library/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
+++ src/dotnet/library/QuestPDF/Skia/SkNativeDependencyCompatibilityChecker.cs
@@ -7,7 +7,7 @@ namespace QuestPDF.Skia;
 
 internal static class SkNativeDependencyCompatibilityChecker
 {
-    private const int ExpectedNativeLibraryVersion = 25;
+    private const int ExpectedNativeLibraryVersion = 26;
     
     private static NativeDependencyCompatibilityChecker Instance { get; } = new()
     {
